64b8eacaf53a0614902a823bc84b76a39aaeebc5
dark_byte <dark_byte@16004826-78b5-1967-6829-2a7ac34a5c3b>
2009-08-26 22:20:45 +0000

    adding the debuggertest to the svn

---
+++ Cheat Engine/debuggertest/Unit1.dfm
@@ -0,0 +1,110 @@
+object Form1: TForm1
+  Left = 302
+  Top = 330
+  Width = 451
+  Height = 251
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 96
+    Top = 53
+    Width = 32
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Label2: TLabel
+    Left = 97
+    Top = 93
+    Width = 32
+    Height = 13
+    Caption = 'Label2'
+  end
+  object Label3: TLabel
+    Left = 200
+    Top = 40
+    Width = 32
+    Height = 13
+    Caption = 'Label3'
+  end
+  object Label4: TLabel
+    Left = 200
+    Top = 56
+    Width = 32
+    Height = 13
+    Caption = 'Label4'
+  end
+  object Label5: TLabel
+    Left = 200
+    Top = 72
+    Width = 32
+    Height = 13
+    Caption = 'Label5'
+  end
+  object Label6: TLabel
+    Left = 200
+    Top = 88
+    Width = 32
+    Height = 13
+    Caption = 'Label6'
+  end
+  object Label7: TLabel
+    Left = 200
+    Top = 104
+    Width = 32
+    Height = 13
+    Caption = 'Label7'
+  end
+  object Label8: TLabel
+    Left = 200
+    Top = 120
+    Width = 32
+    Height = 13
+    Caption = 'Label8'
+  end
+  object Button1: TButton
+    Left = 8
+    Top = 8
+    Width = 137
+    Height = 25
+    Caption = 'Test expected BP'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 8
+    Top = 48
+    Width = 81
+    Height = 25
+    Caption = 'Change health'
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 8
+    Top = 88
+    Width = 81
+    Height = 25
+    Caption = 'Execute code'
+    TabOrder = 2
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 200
+    Top = 8
+    Width = 75
+    Height = 25
+    Caption = 'Get DR'#39's'
+    TabOrder = 3
+    OnClick = Button4Click
+  end
+end
---
+++ Cheat Engine/debuggertest/Unit1.pas
@@ -0,0 +1,176 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Label1: TLabel;
+    Button3: TButton;
+    Label2: TLabel;
+    Button4: TButton;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+  health: dword;
+
+implementation
+
+{$R *.dfm}
+
+procedure x; stdcall;
+var a: dword;
+begin
+  asm
+    nop
+    nop
+    nop
+    nop
+    nop
+  end;
+
+  showmessage('executed and still alive. ebx='+inttohex(a,8));
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var _dr7: dword;
+    _dr0: dword;
+    c: _CONTEXT;
+    p: thandle;
+    d: boolean;
+    x,y,z: dword;
+label XXX;
+begin
+  asm
+    lea eax,[XXX]
+    mov _dr0,eax
+  end;
+
+  _dr7:=$3;
+
+
+  c.ContextFlags:=CONTEXT_DEBUG_REGISTERS;
+  d:=GetThreadContext(GetCurrentThread, c);
+  if not d then showmessage('big shit:'+inttostr(getlasterror));
+
+
+  c.ContextFlags:=CONTEXT_DEBUG_REGISTERS;
+  c.Dr0:=_dr0;
+  c.DR7:=_dr7;
+
+
+
+
+  d:=SetThreadContext(GetCurrentThread, c);
+  if not d then showmessage('shit');
+
+
+
+  try
+    asm
+    mov eax,1
+    mov ebx,2
+    mov ecx,3
+    mov edx,4
+    mov edi,5
+    mov esi,6 
+    {
+    mov ebp,7
+    mov esp,8      }
+
+    mov eax,$123
+    nop
+    nop
+    nop
+
+XXX:
+      nop
+      nop
+      nop
+
+
+      mov z,eax
+      mov x,ebp
+      mov y,esp      
+    end;
+    ShowMessage('Normal exit. Looks like the interrupt was handled(Which under normal situations should not happen). EBP='+inttohex(x,8)+' ESP='+inttohex(y,8)+' EAX='+inttohex(z,8));
+    exit;
+  except
+    on e:exception do
+    begin
+
+      showmessage('breakpoint caused exception. As expected. Message:'+e.message);
+
+    end;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  health:=random(1000);
+  label1.caption:=format('%p : %d',[@health, health]);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  label1.caption:=format('%p : %d',[@health, health]);
+  label2.caption:=format('%p',[@x]);
+  button4.click;
+end;
+
+
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  asm
+    nop
+    push ebx
+    mov ebx,$54321
+    nop
+    nop
+  end;
+  x;
+
+  asm
+    pop ebx
+  end;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+var c: TContext;
+begin
+  c.ContextFlags:=CONTEXT_DEBUG_REGISTERS;
+  if GetThreadContext(GetCurrentThread, c) then
+  begin
+    label3.caption:=inttohex(c.Dr0,8);
+    label4.caption:=inttohex(c.Dr1,8);
+    label5.caption:=inttohex(c.Dr2,8);
+    label6.caption:=inttohex(c.Dr3,8);
+    label7.caption:=inttohex(c.Dr6,8);
+    label8.caption:=inttohex(c.Dr7,8);
+  end;
+end;
+
+end.
