82343653f0fad3c7a5f93abb7e32b580beeb0247
dark_byte <dark_byte@16004826-78b5-1967-6829-2a7ac34a5c3b>
2010-11-27 01:44:34 +0000

    fix incompatibility for lua users

--- Cheat Engine 6/bin/main.lua
+++ Cheat Engine 6/bin/main.lua
@@ -1,3 +1,5 @@
+package.path = package.path .. ";?.lua";
+
 --This lua script gets loaded when Cheat Engine loads
 --You can use this to define some often used functions and libraries you'd like to use
 
