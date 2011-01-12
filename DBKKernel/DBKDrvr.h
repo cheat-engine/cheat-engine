abf1f924023227114c44b55201efc7a73687aa8f
dark_byte <dark_byte@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-01-12 17:51:32 +0000

    make dbkkernel a standalone project

---
+++ DBKKernel/DBKDrvr.h
@@ -0,0 +1,8 @@
+#ifndef DBKDRVR_H
+#define DBKDRVR_H
+
+
+
+#define dbkversion 2000012
+
+#endif
---
+++ DBKKernel/extraimports.h
@@ -0,0 +1,11 @@
+NTSYSAPI
+BOOLEAN
+KeAddSystemServiceTable(
+    IN PULONG_PTR Base,
+    IN PULONG Count OPTIONAL,
+    IN ULONG Limit,
+    IN PUCHAR Number,
+    IN ULONG Index
+    );
+
+
