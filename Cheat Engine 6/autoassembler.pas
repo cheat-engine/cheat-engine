513591c4e670b1fe04477dc1b769b5d35ecc6342
dark_byte <dark_byte@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-01-25 15:01:47 +0000

    aa include: also add support for cedir/include path

--- Cheat Engine 6/autoassembler.pas
+++ Cheat Engine 6/autoassembler.pas
@@ -530,8 +530,15 @@ begin
               if ExtractFileExt(uppercase(s1))='' then
                 s1:=s1+'.CEA';
 
-              s2:=cheatenginedir+s1;
-              if fileexists(s2) then s1:=s2;
+              s2:=cheatenginedir+'includes'+pathdelim+'s1';
+              if fileexists(s2) then s1:=s2 else
+              begin
+                s2:=cheatenginedir+s1;
+                if fileexists(s2) then s1:=s2;
+              end;
+
+
+
 
               if not fileexists(s1) then
                 raise exception.Create(s1+' could not be found');
