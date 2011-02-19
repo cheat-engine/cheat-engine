c385349b1355ac326b74e74b66fa2ca099f6b434
dark_byte <dark_byte@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-02-19 22:36:55 +0000

    add generichotkey to the svn

---
+++ Cheat Engine 6/genericHotkey.pas
@@ -0,0 +1,38 @@
+unit genericHotkey;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, cefuncproc;
+
+type TGenericHotkey=class
+  public
+    keys: TKeycombo;
+    onNotify: TNotifyEvent;
+
+    constructor create(routine: TNotifyEvent; keys: TKeycombo);
+    destructor destroy;
+end;
+
+implementation
+
+uses mainunit, hotkeyhandler;
+
+constructor TGenericHotkey.create(routine: TNotifyEvent; keys: TKeycombo);
+begin
+  //register hotkey
+  onNotify:=routine;
+  self.keys:=keys;
+  RegisterHotKey2(mainform.handle, 0, keys, nil,self);
+end;
+
+destructor TGenericHotkey.destroy;
+begin
+  //unregister hotkey
+  UnregisterGenericHotkey(self);
+end;
+
+end.
+
