7a160d510c63b7e204e30a2ef35199d6326f81ac
dark_byte <dark_byte@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-06-01 22:45:30 +0000

    show hints for the scanoptions

--- Cheat Engine 6/OpenSave.pas
+++ Cheat Engine 6/OpenSave.pas
@@ -14,7 +14,7 @@ uses windows, forms, LCLIntf,registry, SysUtils,AdvancedOptionsUnit,CommentsUnit
      comctrls,dom, xmlread,xmlwrite, FileUtil, ceguicomponents, zstream, luafile, disassemblerComments;
 
 
-var CurrentTableVersion: dword=10;
+var CurrentTableVersion: dword=11;
 procedure protecttrainer(filename: string);
 procedure unprotecttrainer(filename: string; stream: TStream);
 procedure SaveTable(Filename: string; protect: boolean=false);
