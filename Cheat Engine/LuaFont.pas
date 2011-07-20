3edf6636301b1550fed2ea49f9340dbe0ffbd948
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-07-20 21:47:27 +0000

    add font_get/setColor

--- Cheat Engine/LuaFont.pas
+++ Cheat Engine/LuaFont.pas
@@ -11,6 +11,42 @@ procedure initializeLuaFont;
 
 implementation
 
+function font_getColor(L: PLua_State): integer; cdecl;
+var
+  parameters: integer;
+  font: Tfont;
+
+begin
+  result:=0;
+  parameters:=lua_gettop(L);
+  if parameters=1 then
+  begin
+    font:=lua_touserdata(L,-1);
+    lua_pop(L, parameters);
+
+    lua_pushinteger(L, font.Color);
+    result:=1;
+  end else lua_pop(L, parameters);
+end;
+
+function font_setColor(L: PLua_State): integer; cdecl;
+var
+  parameters: integer;
+  font: Tfont;
+  Color: integer;
+begin
+  result:=0;
+  parameters:=lua_gettop(L);
+  if parameters=2 then
+  begin
+    font:=lua_touserdata(L,-parameters);
+    Color:=lua_tointeger(L, -parameters+1);
+    lua_pop(L, parameters);
+
+    font.Color:=Color;
+  end else lua_pop(L, parameters);
+end;
+
 function font_getSize(L: PLua_State): integer; cdecl;
 var
   parameters: integer;
@@ -89,6 +125,8 @@ begin
   lua_register(LuaVM, 'font_setSize', font_setSize);
   lua_register(LuaVM, 'font_getName', font_getName);
   lua_register(LuaVM, 'font_setName', font_setName);
+  lua_register(LuaVM, 'font_getColor', font_getColor);
+  lua_register(LuaVM, 'font_setColor', font_setColor);
 end;
 
 end.
