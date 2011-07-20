28be19579f256631e9625ed9f770d998f6935040
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-07-20 22:25:23 +0000

    fix canvas_draw

--- trunk/Cheat Engine/LuaCanvas.pas
+++ trunk/Cheat Engine/LuaCanvas.pas
@@ -426,7 +426,7 @@ var
 begin
   result:=0;
   parameters:=lua_gettop(L);
-  if parameters=8 then
+  if parameters=4 then
   begin
     canvas:=lua_touserdata(L,-parameters);
     x:=lua_tointeger(L,-parameters+1);
