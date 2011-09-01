e7378ca0092848dd0c33dfebc2771ea3dfc98bd2
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-09-01 01:50:50 +0000

    add d3d11 support And add the used overlay.fx file used

--- Cheat Engine/Direct x mess/CED3D11Hook/CED3D11Hook.cpp
+++ Cheat Engine/Direct x mess/CED3D11Hook/CED3D11Hook.cpp
@@ -1,10 +1,10 @@
-// CED3D10Hook.cpp : Defines the exported functions for the DLL application.
+// CED3D11Hook.cpp : Defines the exported functions for the DLL application.
 //
 
 #include "stdafx.h"
 
 using namespace std;
-map<ID3D10Device *, DXMessD3D10Handler *> D3D10devices;
+map<ID3D11Device *, DXMessD3D11Handler *> D3D11devices;
 
 //definitions
 struct OverlayVertex{
@@ -13,15 +13,14 @@ struct OverlayVertex{
 };
 
 
-
-HRESULT DXMessD3D10Handler::setupOverlayTexture()
+HRESULT DXMessD3D11Handler::setupOverlayTexture()
 //hmm, I could update it to a x,y,width,height, winhandle method now...
 {
 	//call this each time the resolution changes (when the buffer changes)
 	HRESULT hr;
-	ID3D10Resource *test;
-	ID3D10Texture2D *texturex;
-	D3D10_TEXTURE2D_DESC tdesc;
+	ID3D11Resource *test;
+	ID3D11Texture2D *texturex;
+	D3D11_TEXTURE2D_DESC tdesc;
 	DXGI_SWAP_CHAIN_DESC desc;
 	int i;
 
@@ -35,8 +34,6 @@ HRESULT DXMessD3D10Handler::setupOverlayTexture()
 
 
 	
-
-	
 	if (shared->overlaycount > OverlayCount)
 	{
 		int newcount=shared->overlaycount;
@@ -45,12 +42,12 @@ HRESULT DXMessD3D10Handler::setupOverlayTexture()
 		if (overlays==NULL) //initial alloc
 		{
 			
-			overlays=(OverlayData10 *)malloc(sizeof(OverlayData10)* newcount);			
+			overlays=(OverlayData *)malloc(sizeof(OverlayData)* newcount);			
 		}
 		else
 		{
 			//realloc
-			overlays=(OverlayData10 *)realloc(overlays, sizeof(OverlayData10)* newcount);			
+			overlays=(OverlayData *)realloc(overlays, sizeof(OverlayData)* newcount);			
 		}
 
 		
@@ -79,7 +76,7 @@ HRESULT DXMessD3D10Handler::setupOverlayTexture()
 						overlays[i].pOverlayTex=NULL; //should always happen
 				}
 
-				hr=D3DX10CreateTextureFromMemory(dev, (void *)(uintptr_t(shared)+shared->resources[i].resourceoffset), shared->resources[i].resourcesize, NULL, NULL, &test, NULL);
+				hr=D3DX11CreateTextureFromMemory(dev, (void *)(uintptr_t(shared)+shared->resources[i].resourceoffset), shared->resources[i].resourcesize, NULL, NULL, &test, NULL);
 				if( FAILED( hr ) )
 				{
 					OutputDebugStringA("Failure creating a texture");
@@ -87,7 +84,7 @@ HRESULT DXMessD3D10Handler::setupOverlayTexture()
 				}
 
 				//not sure if this is needed or if I can just pass the texturex as resource for resourceview
-				hr=test->QueryInterface(__uuidof(ID3D10Texture2D), (void **)(&texturex));			
+				hr=test->QueryInterface(__uuidof(ID3D11Texture2D), (void **)(&texturex));			
 
 				hr=dev->CreateShaderResourceView(test, NULL, &overlays[i].pOverlayTex);
 				if( FAILED( hr ) )
@@ -132,14 +129,14 @@ HRESULT DXMessD3D10Handler::setupOverlayTexture()
 					{XMFLOAT3(left, top, 0.0f), XMFLOAT2( 0.0f, 0.0f )},		
 				};
 
-				D3D10_BUFFER_DESC bd2d;
+				D3D11_BUFFER_DESC bd2d;
 				ZeroMemory( &bd2d, sizeof(bd2d) );
-				bd2d.Usage = D3D10_USAGE_DYNAMIC;
+				bd2d.Usage = D3D11_USAGE_DYNAMIC;
 				bd2d.ByteWidth = sizeof( OverlayVertex ) * 6;
-				bd2d.BindFlags = D3D10_BIND_VERTEX_BUFFER;
-				bd2d.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+				bd2d.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+				bd2d.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 			   
-				D3D10_SUBRESOURCE_DATA InitData2d;
+				D3D11_SUBRESOURCE_DATA InitData2d;
 				ZeroMemory( &InitData2d, sizeof(InitData2d) );
 				InitData2d.pSysMem = overlayVertices;
 				hr = dev->CreateBuffer( &bd2d, &InitData2d, &overlays[i].pOverlayVB );
@@ -161,18 +158,14 @@ HRESULT DXMessD3D10Handler::setupOverlayTexture()
 
 }
 
-DXMessD3D10Handler::~DXMessD3D10Handler()
+DXMessD3D11Handler::~DXMessD3D11Handler()
 {
 	dev->Release();	
 }
 
-DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD3DHookShared shared)
+DXMessD3D11Handler::DXMessD3D11Handler(ID3D11Device *dev, IDXGISwapChain *sc, PD3DHookShared shared)
 {
 	HRESULT hr;
-	int i;
-
-
-	//hr=D3DX10CreateSprite(dev, 0, &sprite);
 
 	pOverlayIB=NULL;
 
@@ -199,8 +192,8 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 	dev->AddRef();
 	sc->AddRef();
 
-	D3D10_BUFFER_DESC bd2d;
-	D3D10_SUBRESOURCE_DATA InitData2d;
+	D3D11_BUFFER_DESC bd2d;
+	D3D11_SUBRESOURCE_DATA InitData2d;
 
 	
     WORD overlayIndices[] =
@@ -211,9 +204,9 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 
 	ZeroMemory( &bd2d, sizeof(bd2d) );
 
-    bd2d.Usage = D3D10_USAGE_DEFAULT;
+    bd2d.Usage = D3D11_USAGE_DEFAULT;
     bd2d.ByteWidth = sizeof( WORD ) * 6;
-    bd2d.BindFlags = D3D10_BIND_INDEX_BUFFER;
+    bd2d.BindFlags = D3D11_BIND_INDEX_BUFFER;
     bd2d.CPUAccessFlags = 0;
     InitData2d.pSysMem = overlayIndices;
     hr = dev->CreateBuffer( &bd2d, &InitData2d, &pOverlayIB );
@@ -232,9 +225,7 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 	char shaderfile[MAX_PATH];
 	sprintf_s(shaderfile,MAX_PATH, "%s%s", shared->CheatEngineDir,"overlay.fx");
 
-
-
-	hr=D3DX10CompileFromFileA( shaderfile, NULL, NULL, "PS", "ps_4_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, NULL, &pBlob, &pErrorBlob, NULL );
+	hr=D3DX11CompileFromFileA( shaderfile, NULL, NULL, "PS", "ps_4_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, NULL, &pBlob, &pErrorBlob, NULL );
 
 	if (pErrorBlob) 
 		pErrorBlob->Release();
@@ -245,7 +236,7 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 		return;
 	}
 
-    hr = dev->CreatePixelShader( pBlob->GetBufferPointer(), pBlob->GetBufferSize(), &pPixelShader );
+    hr = dev->CreatePixelShader( pBlob->GetBufferPointer(), pBlob->GetBufferSize(), NULL, &pPixelShader );
 	pBlob->Release();
 	if( FAILED( hr ) )
 	{
@@ -256,7 +247,7 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 	pBlob = NULL;
 	pErrorBlob = NULL;
 
-	hr=D3DX10CompileFromFileA( shaderfile, NULL, NULL, "VS", "vs_4_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, NULL, &pBlob, &pErrorBlob, NULL );
+	hr=D3DX11CompileFromFileA( shaderfile, NULL, NULL, "VS", "vs_4_0", D3DCOMPILE_ENABLE_STRICTNESS, 0, NULL, &pBlob, &pErrorBlob, NULL );
 
 	if (pErrorBlob) 
 		pErrorBlob->Release();
@@ -267,7 +258,7 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 		return;
 	}
 
-    hr = dev->CreateVertexShader( pBlob->GetBufferPointer(), pBlob->GetBufferSize(), &pVertexShader );
+    hr = dev->CreateVertexShader( pBlob->GetBufferPointer(), pBlob->GetBufferSize(), NULL, &pVertexShader );
 	
 	if( FAILED( hr ) )
 	{
@@ -277,10 +268,10 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 	}
 	
 
-    D3D10_INPUT_ELEMENT_DESC layout[] =
+    D3D11_INPUT_ELEMENT_DESC layout[] =
     {
-        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D10_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D10_INPUT_PER_VERTEX_DATA, 0 },
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
     UINT numElements = ARRAYSIZE( layout );
 
@@ -292,26 +283,26 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 
 
 
-    D3D10_SAMPLER_DESC sampDesc;
+    D3D11_SAMPLER_DESC sampDesc;
     ZeroMemory( &sampDesc, sizeof(sampDesc) );
-    sampDesc.Filter = D3D10_FILTER_MIN_MAG_MIP_LINEAR;
-    sampDesc.AddressU = D3D10_TEXTURE_ADDRESS_WRAP;
-    sampDesc.AddressV = D3D10_TEXTURE_ADDRESS_WRAP;
-    sampDesc.AddressW = D3D10_TEXTURE_ADDRESS_WRAP;
-    sampDesc.ComparisonFunc = D3D10_COMPARISON_NEVER;
+    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
     sampDesc.MinLOD = 0;
-    sampDesc.MaxLOD = D3D10_FLOAT32_MAX;
+    sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
     hr = dev->CreateSamplerState( &sampDesc, &pSamplerLinear );
     if( FAILED( hr ) )
         return;
 
 
 	//rasterizer
-	D3D10_RASTERIZER_DESC rasterizerdesc;
+	D3D11_RASTERIZER_DESC rasterizerdesc;
 	ZeroMemory(&rasterizerdesc, sizeof(rasterizerdesc));
 	
-	rasterizerdesc.FillMode = D3D10_FILL_SOLID;
-	rasterizerdesc.CullMode = D3D10_CULL_NONE;
+	rasterizerdesc.FillMode = D3D11_FILL_SOLID;
+	rasterizerdesc.CullMode = D3D11_CULL_NONE;
 	rasterizerdesc.FrontCounterClockwise = false;
 	rasterizerdesc.DepthBias = 0;
 	rasterizerdesc.DepthBiasClamp = 0.0f;
@@ -329,23 +320,25 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
         return;
 
 
-	D3D10_BLEND_DESC blend;	
+	D3D11_BLEND_DESC blend;
+	
+	D3D11_RENDER_TARGET_BLEND_DESC rtbd;
+	ZeroMemory( &rtbd, sizeof(rtbd) );
 	ZeroMemory( &blend, sizeof(blend) );
 
-	blend.BlendEnable[0]		 = true;
-	blend.SrcBlend				 = D3D10_BLEND_ONE;
-	blend.DestBlend				 = D3D10_BLEND_INV_SRC_ALPHA;
-	blend.BlendOp				 = D3D10_BLEND_OP_ADD;
-	blend.SrcBlendAlpha			 = D3D10_BLEND_ONE;
-	blend.DestBlendAlpha		 = D3D10_BLEND_ZERO;
-	blend.BlendOpAlpha			 = D3D10_BLEND_OP_ADD;
+	rtbd.BlendEnable			 = true;
+	rtbd.SrcBlend				 = D3D11_BLEND_ONE;	
+	rtbd.DestBlend				 = D3D11_BLEND_INV_SRC_ALPHA;	
+	rtbd.BlendOp				 = D3D11_BLEND_OP_ADD;
+	rtbd.SrcBlendAlpha			 = D3D11_BLEND_ONE;
+	rtbd.DestBlendAlpha			 = D3D11_BLEND_ZERO;
+	rtbd.BlendOpAlpha			 = D3D11_BLEND_OP_ADD;
+	rtbd.RenderTargetWriteMask	 = D3D11_COLOR_WRITE_ENABLE_ALL;
 
-	blend.AlphaToCoverageEnable=true;
+	blend.AlphaToCoverageEnable=false;
+	blend.IndependentBlendEnable=true;
+	blend.RenderTarget[0]=rtbd;
 
-	for (i=0; i<8; i++)
-		blend.RenderTargetWriteMask[i]=D3D10_COLOR_WRITE_ENABLE_ALL;
-
-	pTransparency=NULL;
 	hr=dev->CreateBlendState(&blend, &pTransparency);
 	if( FAILED( hr ) )
         return;
@@ -365,7 +358,7 @@ DXMessD3D10Handler::DXMessD3D10Handler(ID3D10Device *dev, IDXGISwapChain *sc, PD
 }
 
 
-void DXMessD3D10Handler::RenderOverlay()
+void DXMessD3D11Handler::RenderOverlay()
 {
 	int i;
 	HRESULT hr;
@@ -374,6 +367,9 @@ void DXMessD3D10Handler::RenderOverlay()
 		
 
 		//render the overlay
+		ID3D11DeviceContext *dc;
+		dev->GetImmediateContext(&dc);
+
 
 		//check if the overlay has an update
 		//if so, first update the texture
@@ -387,70 +383,69 @@ void DXMessD3D10Handler::RenderOverlay()
 		UINT offset = 0;
 		float blendFactor[] = {1.0f, 1.0f, 1.0f, 1.0f};
 
-		ID3D10VertexShader *oldvs=NULL;
-		//ID3D10ClassInstance *oldvsinstances=NULL;
-		//UINT vci_count=0;
+		ID3D11VertexShader *oldvs=NULL;
+		ID3D11ClassInstance *oldvsinstances=NULL;
+		UINT vci_count=0;
 
-		ID3D10PixelShader *oldps=NULL;
-		//ID3D10ClassInstance *oldpsinstances=NULL;
-		//UINT pci_count=0;
+		ID3D11PixelShader *oldps=NULL;
+		ID3D11ClassInstance *oldpsinstances=NULL;
+		UINT pci_count=0;
 
-		ID3D10SamplerState *oldPSSampler=NULL;
-		ID3D10ShaderResourceView *oldPSShaderResource=NULL;
-		ID3D10BlendState *oldBlendState=NULL;
+		ID3D11SamplerState *oldPSSampler=NULL;
+		ID3D11ShaderResourceView *oldPSShaderResource=NULL;
+		ID3D11BlendState *oldBlendState=NULL;
 		float oldblendFactor[4];
 		UINT oldblendsamplemask;
 
-		ID3D10DepthStencilState *oldDepthStencilState=NULL;
+		ID3D11DepthStencilState *oldDepthStencilState=NULL;
 		UINT oldstencilref;
 
-		D3D10_PRIMITIVE_TOPOLOGY oldPrimitiveTopology;
+		D3D11_PRIMITIVE_TOPOLOGY oldPrimitiveTopology;
 
-		ID3D10InputLayout *oldInputLayout=NULL;
-		ID3D10Buffer *oldIndexBuffer=NULL;
+		ID3D11InputLayout *oldInputLayout=NULL;
+		ID3D11Buffer *oldIndexBuffer=NULL;
 		DXGI_FORMAT oldIndexBufferFormat;
 		UINT oldIndexBufferOffset;
 
-		ID3D10Buffer *oldVertexBuffer=NULL;
+		ID3D11Buffer *oldVertexBuffer=NULL;
 		UINT oldVertexBufferStrides;
 		UINT oldVertexBufferOffset;
 
-		ID3D10RasterizerState *oldRastersizerState=NULL;
+		ID3D11RasterizerState *oldRastersizerState=NULL;
 
 		//save state
 
-		
-		dev->VSGetShader( &oldvs);
-		dev->PSGetShader( &oldps);
-		dev->PSGetSamplers(0,1, &oldPSSampler);
-		dev->PSGetShaderResources(0,1, &oldPSShaderResource);
+		dc->VSGetShader( &oldvs, &oldvsinstances, &vci_count);
+		dc->PSGetShader( &oldps, &oldpsinstances, &pci_count);
+		dc->PSGetSamplers(0,1, &oldPSSampler);
+		dc->PSGetShaderResources(0,1, &oldPSShaderResource);
 
-		dev->OMGetBlendState( &oldBlendState, oldblendFactor, &oldblendsamplemask);
-		dev->OMGetDepthStencilState( &oldDepthStencilState, &oldstencilref);
+		dc->OMGetBlendState( &oldBlendState, oldblendFactor, &oldblendsamplemask);
+		dc->OMGetDepthStencilState( &oldDepthStencilState, &oldstencilref);
 
-		dev->IAGetPrimitiveTopology(&oldPrimitiveTopology);
-		dev->IAGetInputLayout(&oldInputLayout);
-		dev->IAGetIndexBuffer( &oldIndexBuffer, &oldIndexBufferFormat, &oldIndexBufferOffset);
-		dev->IAGetVertexBuffers(0,1,&oldVertexBuffer, &oldVertexBufferStrides, &oldVertexBufferOffset);
+		dc->IAGetPrimitiveTopology(&oldPrimitiveTopology);
+		dc->IAGetInputLayout(&oldInputLayout);
+		dc->IAGetIndexBuffer( &oldIndexBuffer, &oldIndexBufferFormat, &oldIndexBufferOffset);
+		dc->IAGetVertexBuffers(0,1,&oldVertexBuffer, &oldVertexBufferStrides, &oldVertexBufferOffset);
 
-		dev->RSGetState(&oldRastersizerState);
+		dc->RSGetState(&oldRastersizerState);
 
 		//change state
 
-	    dev->VSSetShader(pVertexShader);
-		dev->PSSetShader(pPixelShader);
-		dev->PSSetSamplers( 0, 1, &pSamplerLinear );
+	    dc->VSSetShader( pVertexShader, NULL, 0 );
+		dc->PSSetShader( pPixelShader, NULL, 0 );
+		dc->PSSetSamplers( 0, 1, &pSamplerLinear );
 		
 
-		dev->OMSetBlendState(pTransparency, blendFactor, 0xffffffff);
-		dev->OMSetDepthStencilState(NULL,0);
+		dc->OMSetBlendState(pTransparency, blendFactor, 0xffffffff);
+		dc->OMSetDepthStencilState(NULL,0);
 
-		dev->IASetPrimitiveTopology( D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
-		dev->IASetInputLayout( pVertexLayout );
-		dev->IASetIndexBuffer( pOverlayIB, DXGI_FORMAT_R16_UINT, 0 );
+		dc->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
+		dc->IASetInputLayout( pVertexLayout );
+		dc->IASetIndexBuffer( pOverlayIB, DXGI_FORMAT_R16_UINT, 0 );
 		
 
-		dev->RSSetState(pOverlayRasterizer);
+		dc->RSSetState(pOverlayRasterizer);
 
 		
 		for (i=0; i<OverlayCount; i++)
@@ -458,79 +453,53 @@ void DXMessD3D10Handler::RenderOverlay()
 			if (shared->resources[i].valid)
 			{			
 				//set the vertexbuffer and texture and render
-				dev->IASetVertexBuffers( 0, 1, &overlays[i].pOverlayVB, &stride, &offset );
-				dev->PSSetShaderResources( 0, 1, &overlays[i].pOverlayTex );	
+				dc->IASetVertexBuffers( 0, 1, &overlays[i].pOverlayVB, &stride, &offset );
+				dc->PSSetShaderResources( 0, 1, &overlays[i].pOverlayTex );	
 	
 				//render
-				dev->DrawIndexed( 6, 0,0);
-
-				//dev->VSSetShader(NULL);
-
-				D3DXMATRIX m,m2;
-				DXGI_SWAP_CHAIN_DESC scdesc;
-				
-				swapchain->GetDesc(&scdesc);			
-				D3DXMatrixOrthoOffCenterLH(&m2, 0, (float)scdesc.BufferDesc.Width, (float)scdesc.BufferDesc.Height, 0, 0, 1);
-				//sprite->SetProjectionTransform(&m2);
-
-				//sprite->Begin(D3DX10_SPRITE_SAVE_STATE);
-
-				D3DX10_SPRITE s;
-
-				
-				ZeroMemory(&s,sizeof(s));				
-				D3DXMatrixTransformation2D(&s.matWorld, &D3DXVECTOR2(0, 0), 0, &D3DXVECTOR2(shared->resources[i].width, shared->resources[i].height), &D3DXVECTOR2(0, 0), 0, &D3DXVECTOR2(shared->resources[i].width / 2.0f+shared->resources[i].x, shared->resources[i].height / 2.0f+shared->resources[i].y));
-
-
-			//	
-
-				
-
-
-				s.TexCoord=D3DXVECTOR2( 0, 0 );
-				s.TexSize=D3DXVECTOR2( 0.5f, 0.5f );
-				s.ColorModulate=0xffffffff;
-				s.pTexture=overlays[i].pOverlayTex;
-				s.TextureIndex=0;
-
-				
-				dev->RSSetState(pOverlayRasterizer);
-
-				//sprite->DrawSpritesImmediate(&s, 1, 0,0);
-				
-				//sprite->End();
-
+				dc->DrawIndexed( 6, 0,0);
 			}
 		}
 
 		//restore
-		dev->VSSetShader(oldvs);
-		dev->PSSetShader(oldps);
-		dev->PSSetSamplers(0, 1, &oldPSSampler);
-		dev->PSSetShaderResources(0,1, &oldPSShaderResource);
+		dc->VSSetShader(oldvs, (ID3D11ClassInstance *const *)oldvsinstances, vci_count);
+		dc->PSSetShader(oldps, (ID3D11ClassInstance *const *)oldpsinstances, pci_count);
+		dc->PSSetSamplers(0, 1, &oldPSSampler);
+		dc->PSSetShaderResources(0,1, &oldPSShaderResource);
 
-		dev->OMSetBlendState(oldBlendState, oldblendFactor, oldblendsamplemask);
-		dev->OMSetDepthStencilState(oldDepthStencilState, oldstencilref);
+		dc->OMSetBlendState(oldBlendState, oldblendFactor, oldblendsamplemask);
+		dc->OMSetDepthStencilState(oldDepthStencilState, oldstencilref);
 
-		dev->IASetPrimitiveTopology(oldPrimitiveTopology);
-		dev->IASetInputLayout(oldInputLayout);
-		dev->IASetIndexBuffer(oldIndexBuffer, oldIndexBufferFormat, oldIndexBufferOffset);
-		dev->IASetVertexBuffers(0,1,&oldVertexBuffer, &oldVertexBufferStrides, &oldVertexBufferOffset);
+		dc->IASetPrimitiveTopology(oldPrimitiveTopology);
+		dc->IASetInputLayout(oldInputLayout);
+		dc->IASetIndexBuffer(oldIndexBuffer, oldIndexBufferFormat, oldIndexBufferOffset);
+		dc->IASetVertexBuffers(0,1,&oldVertexBuffer, &oldVertexBufferStrides, &oldVertexBufferOffset);
 
-		dev->RSSetState(oldRastersizerState);
+		dc->RSSetState(oldRastersizerState);
+
+		dc->Release(); //lower the referencecount
+
+		//check if the overlay texture needs to be updated, if so, getdc, update, continue
 	}
 
 }
 
 
-void __stdcall D3D10Hook_SwapChain_Present_imp(IDXGISwapChain *swapchain, ID3D10Device *device, PD3DHookShared shared)
+
+void __stdcall D3D11Hook_SwapChain_Present_imp(IDXGISwapChain *swapchain, ID3D11Device *device, PD3DHookShared shared)
 {
 	//look up the controller class for this device
-	if (D3D10devices[device]==NULL)
+	if (D3D11devices[device]==NULL)
 	{
-		DXMessD3D10Handler *dc=new DXMessD3D10Handler(device, swapchain, shared);//create a new devicehandler
-		D3D10devices[device]=dc;
+		//OutputDebugStringA("New\n");
+		//D3D11devices[dev]=1;
+
+		DXMessD3D11Handler *dc=new DXMessD3D11Handler(device, swapchain, shared);//create a new devicehandler
+
+		//add to the map
+		D3D11devices[device]=dc;
+
 	}
-	D3D10devices[device]->RenderOverlay();	
+	D3D11devices[device]->RenderOverlay();			
 
 }
---
+++ Cheat Engine/Direct x mess/CED3D11Hook/CED3D11Hook.h
@@ -0,0 +1,55 @@
+#ifndef DX11HOOK_H
+#define DX11HOOK_H
+
+#include "stdafx.h"
+
+
+
+
+
+typedef struct
+{
+	ID3D11Buffer *pOverlayVB;
+	ID3D11ShaderResourceView *pOverlayTex;
+} OverlayData, *POverlayData;
+
+class DXMessD3D11Handler
+{
+private:
+	PD3DHookShared shared;
+
+	ID3D11Device *dev;
+	IDXGISwapChain *swapchain;
+
+	
+	ID3D11Buffer *pOverlayIB;
+
+	int OverlayCount;
+	OverlayData *overlays;
+
+
+
+	ID3D11PixelShader *pPixelShader;
+	ID3D11VertexShader *pVertexShader;
+	ID3D11InputLayout *pVertexLayout;
+
+	ID3D11SamplerState *pSamplerLinear;
+	ID3D11RasterizerState *pOverlayRasterizer;
+	ID3D11BlendState *pTransparency;
+
+
+
+	BOOL Valid;
+	HRESULT setupOverlayTexture();
+public:
+	DXMessD3D11Handler(ID3D11Device *dev, IDXGISwapChain *sc, PD3DHookShared shared);
+	~DXMessD3D11Handler();
+	void RenderOverlay();
+
+};
+
+
+
+void __stdcall D3D11Hook_SwapChain_Present_imp(IDXGISwapChain *swapchain, ID3D11Device *device, PD3DHookShared shared);
+
+#endif
