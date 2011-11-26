5864070754176c096d079e29e86a8f2a64f4e617
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-11-26 20:35:24 +0000

    fix threading issue with symbol lookup while being added

--- Cheat Engine/symbollisthandler.pas
+++ Cheat Engine/symbollisthandler.pas
@@ -26,6 +26,7 @@ type
 
   TSymbolListHandler=class
   private
+    cs: TMultiReadExclusiveWriteSynchronizer;
     AddressToString: TAvgLvlTree;
     StringToAddress: TAvgLvlTree;
     function A2SCheck(Tree: TAvgLvlTree; Data1, Data2: pointer): integer;
@@ -52,15 +53,22 @@ var search: TCESymbolInfo;
 begin
   result:=nil;
   search.address:=baseaddress;
-  z:=AddressToString.FindNearest(@search);
-  if z<>nil then
-  begin
-    x:=PCESymbolInfo(z.data);
 
-    while (x<>nil) and (x.address<baseaddress) do
-      x:=x.next;
+  cs.Beginread;
+  try
+    z:=AddressToString.FindNearest(@search);
+    if z<>nil then
+    begin
+      x:=PCESymbolInfo(z.data);
 
-    result:=x;
+      while (x<>nil) and (x.address<baseaddress) do
+        x:=x.next;
+
+      result:=x;
+    end;
+
+  finally
+    cs.Endread;
   end;
 end;
 
@@ -73,51 +81,59 @@ begin
   result:=nil;
   search.address:=address;;
 
-  z:=AddressToString.FindNearest(@search);
+  cs.Beginread;
+  try
 
-  if z<>nil then
-  begin
-    //check if it's a match, and if not, check if it's too big or too small
 
-    x:=PCESymbolInfo(z.data);
-    if x.address=address then
+    z:=AddressToString.FindNearest(@search);
+
+    if z<>nil then
     begin
-      result:=x;
-      exit;
-    end
-    else
-    if x.address<address then
-    begin
-      //if too small, check if it fits inside, else try the next one untill x.address>address or x=nil
-      while (x<>nil) and (x.address<=address) do
+      //check if it's a match, and if not, check if it's too big or too small
+
+      x:=PCESymbolInfo(z.data);
+      if x.address=address then
       begin
-        if InRangeQ(address, x.address, x.address+x.size) then
-        begin
-          result:=x;
-          exit;
-        end;
-
-        //still here so not valid
-        x:=x.next;
-      end;
-    end
-    else
-    begin
-      //if too big, check the previous one, until x.address+x.size < address or x=nil
-      while (x<>nil) and (x.address+x.size>address) do
+        result:=x;
+        exit;
+      end
+      else
+      if x.address<address then
       begin
-        if InRangeQ(address, x.address, x.address+x.size) then
+        //if too small, check if it fits inside, else try the next one untill x.address>address or x=nil
+        while (x<>nil) and (x.address<=address) do
         begin
-          result:=x;
-          exit;
-        end;
+          if InRangeQ(address, x.address, x.address+x.size) then
+          begin
+            result:=x;
+            exit;
+          end;
 
-        //still here so not valid
-        x:=x.previous;
+          //still here so not valid
+          x:=x.next;
+        end;
+      end
+      else
+      begin
+        //if too big, check the previous one, until x.address+x.size < address or x=nil
+        while (x<>nil) and (x.address+x.size>address) do
+        begin
+          if InRangeQ(address, x.address, x.address+x.size) then
+          begin
+            result:=x;
+            exit;
+          end;
+
+          //still here so not valid
+          x:=x.previous;
+        end;
       end;
+
+
     end;
 
-
+  finally
+    cs.Endread;
   end;
 end;
 
@@ -127,11 +143,19 @@ var x: TCESymbolInfo;
 begin
   s:=lowercase(s);
   x.s:=pchar(s);
-  z:=StringToAddress.Find(@x);
-  if z<>nil then
-    result:=z.data
-  else
-    result:=nil;
+
+  cs.Beginread;
+  try
+
+    z:=StringToAddress.Find(@x);
+    if z<>nil then
+      result:=z.data
+    else
+      result:=nil;
+
+  finally
+    cs.Endread;
+  end;
 end;
 
 function TSymbolListHandler.AddSymbol(module: string; searchkey: string; address: qword; size: integer; skipaddresstostringlookup: boolean=false): PCESymbolInfo;
@@ -146,30 +170,35 @@ begin
   new.address:=address;
   new.size:=size;
 
-  if not skipaddresstostringlookup then
-  begin
-    n:=AddressToString.Add(new);
-    prev:=AddressToString.FindPrecessor(n);
-    next:=AddressToString.FindSuccessor(n);
-
-    if prev=nil then
-      new.previous:=nil
-    else
+  cs.Beginwrite;
+  try
+    if not skipaddresstostringlookup then
     begin
-      new.previous:=prev.Data;
-      PCESymbolInfo(prev.data).next:=new;
+      n:=AddressToString.Add(new);
+      prev:=AddressToString.FindPrecessor(n);
+      next:=AddressToString.FindSuccessor(n);
+
+      if prev=nil then
+        new.previous:=nil
+      else
+      begin
+        new.previous:=prev.Data;
+        PCESymbolInfo(prev.data).next:=new;
+      end;
+
+      if next=nil then
+        new.next:=nil
+      else
+      begin
+        new.next:=next.Data;
+        PCESymbolInfo(next.data).previous:=new;
+      end;
     end;
 
-    if next=nil then
-      new.next:=nil
-    else
-    begin
-      new.next:=next.Data;
-      PCESymbolInfo(next.data).previous:=new;
-    end;
+    StringToAddress.Add(new);
+  finally
+    cs.Endwrite;
   end;
-
-  StringToAddress.Add(new);
   result:=new;
 end;
 
@@ -188,31 +217,37 @@ procedure TSymbolListHandler.clear;
 var x: TAvgLvlTreeNode;
   d:PCESymbolInfo;
 begin
-  if AddressToString<>nil then
-  begin
-    x:=AddressToString.FindLowest;
-    while x<>nil do
+  cs.Beginwrite;
+  try
+    if AddressToString<>nil then
     begin
-      d:=PCESymbolInfo(x.Data);
+      x:=AddressToString.FindLowest;
+      while x<>nil do
+      begin
+        d:=PCESymbolInfo(x.Data);
 
-      if d.originalstring<>nil then
-        StrDispose(d.originalstring);
+        if d.originalstring<>nil then
+          StrDispose(d.originalstring);
 
-      if d.s<>nil then
-        StrDispose(d.s);
+        if d.s<>nil then
+          StrDispose(d.s);
 
-      if d.module<>nil then
-        strDispose(d.module);
+        if d.module<>nil then
+          strDispose(d.module);
 
-      freemem(d);
-      x:=AddressToString.FindSuccessor(x);
+        freemem(d);
+        x:=AddressToString.FindSuccessor(x);
+      end;
+
+      AddressToString.Clear;
     end;
 
-    AddressToString.Clear;
-  end;
+    if StringToAddress<>nil then
+      StringToAddress.Clear;
 
-  if StringToAddress<>nil then
-    StringToAddress.Clear;
+  finally
+    cs.endwrite;
+  end;
 end;
 
 constructor TSymbolListHandler.create;
@@ -220,16 +255,20 @@ begin
   inherited create;
   AddressToString:=TAvgLvlTree.CreateObjectCompare(A2SCheck);
   StringToAddress:=TAvgLvlTree.CreateObjectCompare(S2ACheck);
+  cs:=TMultiReadExclusiveWriteSynchronizer.create;
 end;
 
 destructor TSymbolListHandler.destroy;
 begin
   clear;
   if AddressToString<>nil then
-    AddressToString.Free;
+    freeandnil(AddressToString);
 
   if StringToAddress<>nil then
-    StringToAddress.Free;
+    freeandnil(StringToAddress);
+
+  if cs<>nil then
+    freeandnil(cs);
 
   inherited destroy;
 end;
