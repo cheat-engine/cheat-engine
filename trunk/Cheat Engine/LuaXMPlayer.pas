3f5b816d6f1d165814b15e8be4261ebd0c862337
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2011-12-21 20:52:14 +0000

    implemented xmplayer_setVolume() for lua added some explanation to the groupscan buildup window

---
+++ trunk/Cheat Engine/LuaXMPlayer.pas
@@ -0,0 +1,119 @@
+unit LuaXMPlayer;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, xmplayer_defines, xmplayer_server, lua, lauxlib, lualib, luafile, LuaHandler;
+
+procedure initializeLuaXMPlayer;
+
+implementation
+
+function xmplayer_playXM(L: Plua_State): integer; cdecl;
+var parameters: integer;
+  lf: TLuaFile;
+  f: string;
+  i: integer;
+begin
+  if xmplayer=nil then
+    xmplayer:=TXMPlayer.create;
+
+  result:=0;
+  parameters:=lua_gettop(L);
+  if (xmplayer<>nil) and (parameters=1) then
+  begin
+    if lua_islightuserdata(L,-1) then //stream
+      xmplayer.playXM(tstream(lua_touserdata(L,-1)))
+    else
+      xmplayer.playXM(Lua_ToString(L,-1))
+  end;
+
+  lua_pop(L, lua_gettop(L));
+end;
+
+function xmplayer_pause(L: Plua_State): integer; cdecl;
+var parameters: integer;
+begin
+  if xmplayer=nil then
+    xmplayer:=TXMPlayer.create;
+
+  result:=0;
+  lua_pop(L, lua_gettop(L));
+
+  if xmplayer<>nil then
+     xmplayer.pause;
+end;
+
+function xmplayer_resume(L: Plua_State): integer; cdecl;
+var parameters: integer;
+begin
+  if xmplayer=nil then
+    xmplayer:=TXMPlayer.create;
+
+  result:=0;
+  lua_pop(L, lua_gettop(L));
+
+  if xmplayer<>nil then
+     xmplayer.resume;
+end;
+
+function xmplayer_stop(L: Plua_State): integer; cdecl;
+var parameters: integer;
+begin
+  if xmplayer=nil then
+    xmplayer:=TXMPlayer.create;
+
+  result:=0;
+  lua_pop(L, lua_gettop(L));
+
+  if xmplayer<>nil then
+     xmplayer.stop;
+end;
+
+function xmplayer_isPlaying(L: Plua_State): integer; cdecl;
+var parameters: integer;
+begin
+  if xmplayer=nil then
+    xmplayer:=TXMPlayer.create;
+
+  result:=0;
+  lua_pop(L, lua_gettop(L));
+
+  if xmplayer<>nil then
+  begin
+    result:=1;
+    lua_pushboolean(L, xmplayer.isPlaying);
+  end;
+end;
+
+function xmplayer_setVolume(L: Plua_State): integer; cdecl;
+var parameters: integer;
+  v: integer;
+begin
+  if xmplayer=nil then
+    xmplayer:=TXMPlayer.create;
+
+  result:=0;
+  parameters:=lua_gettop(L);
+  if (xmplayer<>nil) and (parameters=1) then
+  begin
+    v:=lua_tointeger(L,-1);
+    xmplayer.setVolume(v);
+  end;
+
+  lua_pop(L, lua_gettop(L));
+end;
+
+procedure initializeLuaXMPlayer;
+begin
+  Lua_register(LuaVM, 'xmplayer_playXM', xmplayer_playXM);
+  Lua_register(LuaVM, 'xmplayer_pause', xmplayer_pause);
+  Lua_register(LuaVM, 'xmplayer_resume', xmplayer_resume);
+  Lua_register(LuaVM, 'xmplayer_stop', xmplayer_stop);
+  Lua_register(LuaVM, 'xmplayer_isPlaying', xmplayer_isPlaying);
+end;
+
+end.
+
