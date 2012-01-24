f8933076749848c1edace894beba367c3c30b3fd
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2012-01-24 01:04:23 +0000

    beta 3 time again

--- trunk/Cheat Engine/MainUnit2.pas
+++ trunk/Cheat Engine/MainUnit2.pas
@@ -27,7 +27,7 @@ function GetScanType: TScanOption;
 function GetScanType2: TScanOption;
 
 
-const beta=' Beta 2'; //empty this for a release
+const beta=' Beta 3'; //empty this for a release
 
 var
   CEnorm:string;
