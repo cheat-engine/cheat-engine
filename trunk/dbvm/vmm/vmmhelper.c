ebe27dd5066c65e77ebe75c6b05631c56b17a4ff
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2013-05-08 23:20:11 +0000

    forgot some breaks here

--- trunk/dbvm/vmm/vmmhelper.c
+++ trunk/dbvm/vmm/vmmhelper.c
@@ -76,33 +76,33 @@ char * getVMInstructionErrorString(void)
   char *result;
   switch (i)
   {
-    case 0: result="No error";
-    case 1: result="VMCALL executed in VMX root operation";
-    case 2: result="VMCLEAR with invalid physical address";
-    case 3: result="VMCLEAR with VMXON pointer";
-    case 4: result="VMLAUNCH with non-clear VMCS";
-    case 5: result="VMRESUME with non-launched VMCS";
-    case 6: result="VMRESUME with a corrupted VMCS";
-    case 7: result="VM entry with invalid fields";
-    case 8: result="VM entry with invalid host-state fields";
-    case 9: result="VMPTRLD with invalid physical address";
-    case 10: result="VMPTRLD with VMXON pointer";
-    case 11: result="VMPTRLD with incorrect VMCS revision identifier";
-    case 12: result="VMREAD/VMWRITE from/to unsupported VMCS component";
-    case 13: result="VMWRITE to read-only VMCS component";
-    case 15: result="VMXON executed in VMX root operation";
-    case 16: result="VM entry with invalid executive-VMCS pointer";
-    case 17: result="VM entry with non-launched executive-VMCS pointer";
-    case 18: result="VM entry with executive-VMCS pointer but not VMXON pointer";
-    case 19: result="VMCALL with non-clear VMCS";
-    case 20: result="VMCALL with invalid VM-exit control fields";
-    case 22: result="VMCALL with incorrect MSEG revision number";
-    case 23: result="VMXOFF under dual-monitor treatment of SMIs and SMM";
-    case 24: result="VMCALL with invalid SMM-monitor features";
-    case 25: result="VM entry with invalid VM-execution control fields in executive VMCS";
-    case 26: result="VM entry with events blocked by MOV SS";
+    case 0: result="No error"; break;
+    case 1: result="VMCALL executed in VMX root operation"; break;
+    case 2: result="VMCLEAR with invalid physical address"; break;
+    case 3: result="VMCLEAR with VMXON pointer"; break;
+    case 4: result="VMLAUNCH with non-clear VMCS"; break;
+    case 5: result="VMRESUME with non-launched VMCS"; break;
+    case 6: result="VMRESUME with a corrupted VMCS"; break;
+    case 7: result="VM entry with invalid fields"; break;
+    case 8: result="VM entry with invalid host-state fields"; break;
+    case 9: result="VMPTRLD with invalid physical address"; break;
+    case 10: result="VMPTRLD with VMXON pointer"; break;
+    case 11: result="VMPTRLD with incorrect VMCS revision identifier"; break;
+    case 12: result="VMREAD/VMWRITE from/to unsupported VMCS component"; break;
+    case 13: result="VMWRITE to read-only VMCS component"; break;
+    case 15: result="VMXON executed in VMX root operation"; break;
+    case 16: result="VM entry with invalid executive-VMCS pointer"; break;
+    case 17: result="VM entry with non-launched executive-VMCS pointer"; break;
+    case 18: result="VM entry with executive-VMCS pointer but not VMXON pointer"; break;
+    case 19: result="VMCALL with non-clear VMCS"; break;
+    case 20: result="VMCALL with invalid VM-exit control fields"; break;
+    case 22: result="VMCALL with incorrect MSEG revision number"; break;
+    case 23: result="VMXOFF under dual-monitor treatment of SMIs and SMM"; break;
+    case 24: result="VMCALL with invalid SMM-monitor features"; break;
+    case 25: result="VM entry with invalid VM-execution control fields in executive VMCS"; break;
+    case 26: result="VM entry with events blocked by MOV SS"; break;
 
-    default: result="Undefined";
+    default: result="Undefined"; break;
   }
 
 
@@ -1544,10 +1544,12 @@ int vmexit(pcpuinfo currentcpuinfo, UINT64 *registers)
 
         *(unsigned char *)address=bt;
         sendstring("\n\r");
+        break;
       }
 
       default:
         sendstring("Unknown command\n\r");
+        break;
   	}
   }
 
