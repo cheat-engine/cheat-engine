254e670dac2c906e7a161799702dc462d60a2281
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2013-07-11 10:47:55 +0000

    test and fix watchpoint breakpoints for arm (find what writes/accesses ....) note to self: Opposed to intel, these breakpoints seem to break before execution

--- trunk/Cheat Engine/ceserver/ceservertest.c
+++ trunk/Cheat Engine/ceserver/ceservertest.c
@@ -259,7 +259,8 @@ void *CESERVERTEST_DEBUGGERTHREAD(void *arg)
       {
         printf("going to set breakpoint\n");
 #ifdef __arm__
-        cenet_setBreakpoint(fd, pHandle, -1, 0x8374, 0, 1);
+        //cenet_setBreakpoint(fd, pHandle, -1, 0x8374, 0, 1);
+        cenet_setBreakpoint(fd, pHandle, -1, 0xa000, 1, 4);
 #else
         cenet_setBreakpoint(fd, pHandle, -1, 0x4007ad, 0, 1);
 #endif
@@ -274,10 +275,10 @@ void *CESERVERTEST_DEBUGGERTHREAD(void *arg)
         if (devent.debugevent==5)
         {
           printf("TRAP (thread %d)\n", devent.threadid);
-          printf("Going to remove breakpoint\n");
-          cenet_removeBreakpoint(fd, pHandle, devent.threadid);
+         // printf("Going to remove breakpoint\n");
+         // cenet_removeBreakpoint(fd, pHandle, devent.threadid);
 
-          printf("cenet_removeBreakpoint returned\n");
+         // printf("cenet_removeBreakpoint returned\n");
           cenet_continueFromDebugEvent(fd, pHandle, devent.threadid, 1); //continue unhandled/single step
         }
         else
