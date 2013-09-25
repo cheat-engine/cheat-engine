b7c1fa5f41292d38f9e003c59ab6d362b7c3c55c
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2013-09-25 15:12:15 +0000

    improve pointer rescan speed a bit, and combine all rpm calls into one location

--- trunk/Cheat Engine/rescanhelper.pas
+++ trunk/Cheat Engine/rescanhelper.pas
@@ -4,57 +4,32 @@ unit rescanhelper;
 
 interface
 
-uses windows, LCLIntf, classes, symbolhandler, CEFuncProc,NewKernelHandler;
-
-type TPointerAddress=record
-  value: ptrUint;
-end;
-
+uses windows, LCLIntf, classes, symbolhandler, CEFuncProc,NewKernelHandler, maps, sysutils, syncobjs;
 
 type
-  PPointerAddress=^TPointerAddress;
-
-  PPointerTable=^TPointerTable;
-  PPointerListArray=^TPointerListArray;
-  TPointerTable=record
-    case integer of
-      1: (pointeraddress: PPointerAddress); //if this is the last level (maxlevel) this is an PPointerList
-      2: (PointerlistArray: PPointerListArray);   //else it's a PReversePointerListArray
+  TPageInfo=record
+    data: PByteArray;
   end;
-  TPointerListArray=array [0..15] of TPointerTable;
 
+  PPageInfo=^TPageInfo;
 
-
-type
-  PAddressTable=^TAddressTable;
-  PAddressListArray=^TAddressListArray;
-  TAddressTable=record
-    case integer of
-      1: (addressvalue: pointer); //if this is the last level (maxlevel) this is a pointer to a type of the value to scan for (4 byute and single are 4 byte, double is 8 byte)
-      2: (AddresslistArray: PAddressListArray);   //else it's a PReversePointerListArray
-  end;
-  TAddressListArray=array [0..15] of TAddressTable;
-
-type TRescanHelper=class
+  TRescanHelper=class
   private
-    level0list: PPointerListArray;
-    addresslist: PAddressListArray;
-    dne: TPointerAddress;
+    pagemap: TMap;
+    pagemapcs: TCriticalsection;
+
     memoryregion: array of TMemoryRegion;
-    maxlevel: integer;
+
     function BinSearchMemRegions(address: ptrUint): integer;
-    procedure DeletePath(addresslist: PPointerListArray; level: integer);
-    procedure DeleteAddressPath(addresslist: PAddressListArray; level: integer);
     procedure quicksortmemoryregions(lo,hi: integer);
   public
     function ispointer(address: ptrUint): boolean;
-    function AddPointer(Address: ptrUint; value: ptrUint): PPointerAddress;
-    function findPointer(Address: ptrUint): PPointerAddress;
+    function GetMemoryRegion(Address: ptruint): TMemoryRegion;
 
-    function AddAddress(Address: ptrUint; value: pointer; valuesize: integer): pointer;
-    function findAddress(Address: ptrUint): pointer;
+    function FindPage(index: ptruint): TPageInfo;
 
-    constructor create;
+
+    constructor create(valuesize: integer; memoryregions: PMemoryRegions=nil);
     destructor destroy; override;
 end;
 
@@ -101,202 +76,70 @@ begin
   result:=BinSearchMemRegions(address)<>-1;
 end;
 
+function TRescanHelper.GetMemoryRegion(Address: ptruint): TMemoryRegion;
+var i: integer;
+begin
+  i:=BinSearchMemRegions(address);
+  if i<>-1 then
+    result:=memoryregion[BinSearchMemRegions(address)]
+  else
+    result.BaseAddress:=0;
+end;
 
-function TRescanHelper.findPointer(Address: ptrUint): PPointerAddress;
+function TRescanHelper.findPage(Index: ptrUint): TPageInfo;
 {
-Called by a rescanworker
-If it returns nil, it isn't in the list and the rescanworker will have to read it itself
-if it's not nil the PointerAddress contains a dword. If it's 0, the address is not readable
+will find the pageinfo. If not found, it will add it
 }
 var
-  level: integer;
-  entrynr: integer;
-  currentarray: PPointerListArray;
+  pi: TPageInfo;
+  x: dword;
+
+  r: PPageInfo;
 begin
-  //first check if this address actually exists inside a memory region, if not, just tell it the "does not exist" pointeraddress
-  result:=nil;
-  if not ispointer(address) then
-    result:=@dne
-  else
+
+  //the old method is faster, but has a small threading issue where adding new entries would lead to a memory leak if multiple threads added the same thing
+
+  r:=pagemap.GetDataPtr(index);
+
+
+  if r=nil then
   begin
-    //find the exact address
-    level:=0;
-    currentarray:=level0list;
-    while level<maxlevel do
+    //not yet added, add it to the list
+    pi.data:=nil;
+    pagemapcs.enter; //adding on the other hand does require a lock, as it needs to fidn out where to add it, and what variables to initialize to what value (next/previous)
+
+    r:=pagemap.GetDataPtr(index);
+
+    if r=nil then
     begin
-      entrynr:=address shr ((maxlevel-level)*4) and $f;
-      if currentarray[entrynr].PointerlistArray=nil then exit; //not found
-
-      currentarray:=currentarray[entrynr].PointerlistArray;
-      inc(level);
-    end;
-
-    entrynr:=address shr ((maxlevel-level)*4) and $f;
-    if currentarray[entrynr].pointeraddress<>nil then
-      result:=currentarray[entrynr].pointeraddress;
-  end;
-end;
-
-function TRescanHelper.AddPointer(Address: ptrUint; value: ptrUint): PPointerAddress;
-{Called by the rescanworker when it has read a pointer and findpointer returned NIL }
-{No mutex is used here as no thread is allowed to REMOVE items, and the pointers are only set after the afterlying objects have been created}
-var
-  level: integer;
-  entrynr: integer;
-  temp, currentarray: PPointerListArray;
-  temp2: PPointeraddress;
-begin
-  currentarray:=level0list;
-
-  level:=0;
-  while level<maxlevel do
-  begin
-    //add the path if needed
-    entrynr:=address shr ((maxlevel-level)*4) and $f;
-    if currentarray[entrynr].PointerlistArray=nil then //allocate
-    begin
-      getmem(temp, sizeof(TPointerListArray));
-      ZeroMemory(temp, sizeof(TPointerListArray));
-      //the list has been created and initialized, tell the others:
-      currentarray[entrynr].PointerlistArray:=temp;
-    end;
-
-    currentarray:=currentarray[entrynr].PointerlistArray;
-    inc(level);
-  end;
-
-  //got till level maxlevel
-  entrynr:=address shr ((maxlevel-level)*4) and $f;
-  if currentarray[entrynr].pointeraddress=nil then //allocate one
-  begin
-    getmem(temp2, sizeof(TPointerAddress));
-    temp2.value:=value;
-    currentarray[entrynr].pointeraddress:=temp2; //publish
-    result:=temp2;
-  end else result:=currentarray[entrynr].pointeraddress;
-end;
-
-function TRescanHelper.findAddress(Address: ptrUint): pointer;
-var
-  level: integer;
-  entrynr: integer;
-  currentarray: PAddressListArray;
-begin
-  //first check if this address actually exists inside a memory region, if not, just tell it the "does not exist" pointeraddress
-  result:=nil;
-
-  //find the exact address
-  level:=0;
-  currentarray:=addresslist;
-  while level<maxlevel do
-  begin
-    entrynr:=address shr ((maxlevel-level)*4) and $f;
-    if currentarray[entrynr].AddresslistArray=nil then exit; //not found
-
-    currentarray:=currentarray[entrynr].AddresslistArray;
-    inc(level);
-  end;
-
-  entrynr:=address shr ((maxlevel-level)*4) and $f;
-  if currentarray[entrynr].addressvalue<>nil then
-    result:=currentarray[entrynr].addressvalue;
-end;
-
-function TRescanHelper.AddAddress(Address: ptrUint; value: pointer; valuesize: integer): pointer;
-var
-  level: integer;
-  entrynr: integer;
-  temp, currentarray: PAddressListArray;
-  temp2: pointer;
-begin
-  currentarray:=addresslist;
-
-  level:=0;
-  while level<maxlevel do
-  begin
-    //add the path if needed
-    entrynr:=address shr ((maxlevel-level)*4) and $f;
-    if currentarray[entrynr].addresslistArray=nil then //allocate
-    begin
-      getmem(temp, sizeof(TaddressListArray));
-      ZeroMemory(temp, sizeof(TaddressListArray));
-      //the list has been created and initialized, tell the others:
-      currentarray[entrynr].addresslistArray:=temp;
-    end;
-
-    currentarray:=currentarray[entrynr].addresslistArray;
-    inc(level);
-  end;
-
-  //got till level 7
-  entrynr:=address shr ((maxlevel-level)*4) and $f;
-  if currentarray[entrynr].addressvalue=nil then //allocate one
-  begin
-    getmem(temp2, valuesize);
-    copymemory(temp2,value,valuesize);
-    currentarray[entrynr].addressvalue:=temp2; //publish
-    result:=temp2;
-  end else result:=currentarray[entrynr].addressvalue;
-end;
-
-
-procedure TRescanHelper.DeletePath(addresslist: PPointerListArray; level: integer);
-var
-  i: integer;
-begin
-  if level=maxlevel then
-  begin
-    for i:=0 to $F do
-    begin
-      if addresslist[i].pointeraddress<>nil then
+      //not yet added by another thread
+      if ispointer(index shl 12) then
       begin
-        freemem(addresslist[i].pointeraddress);
-        addresslist[i].pointeraddress:=nil;
+        //save the data
+        getmem(pi.data, 4096);
+        if ReadProcessMemory(ProcessHandle, pointer(index shl 12), pi.data, 4096, x)=false then //unexpected failure reading the memory
+          freemem(pi.data);
       end;
+
+      //add it
+      pagemap.Add(index, pi);
+
+      result:=pi;
+    end
+    else
+    begin
+      //another thread added it
+      result:=r^;
     end;
+
+
+    pagemapcs.Leave;
+
   end
-  else
-  begin
-    for i:=0 to $F do
-    begin
-      if addresslist[i].PointerlistArray<>nil then
-      begin
-        deletepath(addresslist[i].PointerlistArray,level+1);
-        freemem(addresslist[i].PointerlistArray);
-        addresslist[i].PointerlistArray:=nil;
-      end;
-    end;
-  end;
+  else //found:
+    result:=r^;
 end;
 
-procedure TRescanHelper.DeleteAddressPath(addresslist: PAddressListArray; level: integer);
-var
-  i: integer;
-begin
-  if level=maxlevel then
-  begin
-    for i:=0 to $F do
-    begin
-      if addresslist[i].addressvalue<>nil then
-      begin
-        freemem(addresslist[i].addressvalue);
-        addresslist[i].addressvalue:=nil;
-      end;
-    end;
-  end
-  else
-  begin
-    for i:=0 to $F do
-    begin
-      if addresslist[i].AddresslistArray<>nil then
-      begin
-        deleteAddresspath(addresslist[i].AddresslistArray,level+1);
-        freemem(addresslist[i].AddresslistArray);
-        addresslist[i].AddresslistArray:=nil;
-      end;
-    end;
-  end;
-end;
 
 procedure TRescanHelper.quicksortmemoryregions(lo,hi: integer);
 var i,j: integer;
@@ -326,14 +169,20 @@ begin
   if (i<hi) then quicksortmemoryregions(i,hi);
 end;
 
-constructor TRescanHelper.create;
+constructor TRescanHelper.create(valuesize: integer; memoryregions: PMemoryRegions=nil);
 var
   mbi : _MEMORY_BASIC_INFORMATION;
   address: ptrUint;
   i: integer;
 begin
-  dne.value:=0;
 
+  pagemap:=TMap.Create(ituPtrSize, sizeof(TPageInfo));
+  pagemapcs:=TCriticalSection.create;
+
+
+
+
+          {
   getmem(level0list, sizeof(TPointerListArray));
   ZeroMemory(level0list, sizeof(TPointerListArray));
 
@@ -341,61 +190,68 @@ begin
   ZeroMemory(addresslist, sizeof(TAddressListArray));
 
   if processhandler.is64Bit then maxlevel:=15 else maxlevel:=7;
-
+                    }
 
   //enumerate all memory regions
   address:=0;
 
 
-  while (Virtualqueryex(processhandle,pointer(address),mbi,sizeof(mbi))<>0) and ((address+mbi.RegionSize)>address) do
+  if memoryregions=nil then
   begin
-    if (not symhandler.inSystemModule(ptrUint(mbi.baseAddress))) and (not (not scan_mem_private and (mbi._type=mem_private))) and (not (not scan_mem_image and (mbi._type=mem_image))) and (not (not scan_mem_mapped and ((mbi._type and mem_mapped)>0))) and (mbi.State=mem_commit) and ((mbi.Protect and page_guard)=0) and ((mbi.protect and page_noaccess)=0) then  //look if it is commited
+
+    while (Virtualqueryex(processhandle,pointer(address),mbi,sizeof(mbi))<>0) and ((address+mbi.RegionSize)>address) do
     begin
-      if Skip_PAGE_NOCACHE then
-        if (mbi.AllocationProtect and PAGE_NOCACHE)=PAGE_NOCACHE then
-        begin
-          address:=ptrUint(mbi.BaseAddress)+mbi.RegionSize;
-          continue;
-        end;
+      if (not symhandler.inSystemModule(ptrUint(mbi.baseAddress))) and (not (not scan_mem_private and (mbi._type=mem_private))) and (not (not scan_mem_image and (mbi._type=mem_image))) and (not (not scan_mem_mapped and ((mbi._type and mem_mapped)>0))) and (mbi.State=mem_commit) and ((mbi.Protect and page_guard)=0) and ((mbi.protect and page_noaccess)=0) then  //look if it is commited
+      begin
+        if Skip_PAGE_NOCACHE then
+          if (mbi.AllocationProtect and PAGE_NOCACHE)=PAGE_NOCACHE then
+          begin
+            address:=ptrUint(mbi.BaseAddress)+mbi.RegionSize;
+            continue;
+          end;
 
-      setlength(memoryregion,length(memoryregion)+1);
+        setlength(memoryregion,length(memoryregion)+1);
 
-      memoryregion[length(memoryregion)-1].BaseAddress:=ptrUint(mbi.baseaddress);  //just remember this location
-      memoryregion[length(memoryregion)-1].MemorySize:=mbi.RegionSize;
+        memoryregion[length(memoryregion)-1].BaseAddress:=ptrUint(mbi.baseaddress);  //just remember this location
+        memoryregion[length(memoryregion)-1].MemorySize:=mbi.RegionSize;
+      end;
+
+
+      address:=ptrUint(mbi.baseaddress)+mbi.RegionSize;
     end;
+  end
+  else
+    self.memoryregion:=memoryregions^;
 
-
-    address:=ptrUint(mbi.baseaddress)+mbi.RegionSize;
-  end;
-
-  //split up the memory regions into small chunks of max 512KB (so don't allocate a fucking 1GB region)
-  i:=0;
-  while i<length(memoryregion) do
-  begin
-    if memoryregion[i].MemorySize>512*1024 then
-    begin
-      //too big, so cut into pieces
-      //create new entry with 512KB less
-      setlength(memoryregion,length(memoryregion)+1);
-      memoryregion[length(memoryregion)-1].BaseAddress:=memoryregion[i].BaseAddress+512*1024;
-      memoryregion[length(memoryregion)-1].MemorySize:=memoryregion[i].MemorySize-512*1024;
-      memoryregion[i].MemorySize:=512*1024; //set the current region to be 512KB
-
-    end;
-    inc(i); //next item. Eventually the new items will be handled, and they will also be split untill the list is finally cut into small enough chunks    
-  end;
 
   //sort memoryregions from small to high
-  quicksortmemoryregions(0,length(memoryregion)-1);  
-  
+  quicksortmemoryregions(0,length(memoryregion)-1);
+
 end;
 
 destructor TRescanHelper.destroy;
+var i: integer;
+  pmi: TMapIterator;
 begin
   setlength(memoryregion,0);
-  deletepath(level0list,0);
 
-  deleteAddressPath(addresslist,0);
+  pagemapcs.free;
+
+  pmi:=TMapIterator.Create(pagemap);
+  pmi.First;
+  while not pmi.EOM do
+  begin
+    if pmi.DataPtr<>nil then
+    begin
+      if (PPageInfo(pmi.DataPtr).data<>nil) then
+        freemem(PPageInfo(pmi.DataPtr).data);
+    end;
+    pmi.Next;
+  end;
+
+  pagemap.Clear;
+  pagemap.Free;
+
 
   inherited destroy;
 end;
