1de9c57119c9e79d56fc245fafa3288666741bb0
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2014-02-07 15:20:54 +0000

    Add access function to the user registry environment variables

--- trunk/Cheat Engine/bin/main.lua
+++ trunk/Cheat Engine/bin/main.lua
@@ -289,6 +289,10 @@ getPreviousOpcode(address): Returns the address of the previous opcode (this is 
 
 beep() : Plays the fabulous beep/ping sound!
 
+getUserRegistryEnvironmentVariable(name): string - Returns the environment variable stored in the user registry environment
+setUserRegistryEnvironmentVariable(name, string) - Sets the environment variable stored in the user registry environment
+broadcastEnvironmentUpdate() : Call this when you've changed the environment variables in the registry. This will cause at least the shell to update so you don't have to reboot. (It's always recommended to reboot though)
+
 
 getFormCount() : Returns the total number of forms assigned to the main CE application
 getForm(index): Returns the form at the specific index
