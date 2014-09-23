ad9fae0d141cbaeb564992771bfe68791f236f03
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2014-09-23 10:03:39 +0000

    commit some stuff before major overhauling a big piece of code

---
+++ trunk/Cheat Engine/pointerscanworker.pas
@@ -0,0 +1,14 @@
+unit PointerscanWorker;
+//todo: try splitting up the pointerscanner into smaller unit files
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
