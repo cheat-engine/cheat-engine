dde46829fd123a34606ff40d65e74cf102832cb5
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2015-01-31 15:01:30 +0000

    forgot to add this file to the svn

---
+++ trunk/Cheat Engine/jnitfoundlist.pas
@@ -0,0 +1,124 @@
+unit jnitfoundlist;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, jni;
+
+procedure InitializeJniTFoundList(env: PJNIEnv);
+
+implementation
+
+uses unixporthelper, jniTObject, foundlisthelper, memscan;
+
+
+function TFoundList_initialize(PEnv: PJNIEnv; Obj: JObject): jlong; cdecl;
+var fl: TFoundList;
+begin
+  result:=0;
+
+  log('TFoundList_initialize');
+  fl:=TFoundList(JObjectToTObject(penv, obj));
+
+  if fl<>nil then
+  begin
+    try
+      result:=fl.Initialize;
+    except
+      on e:exception do
+      begin
+        log(e.Message);
+      end;
+    end;
+  end
+  else
+    log('fl is nil');
+
+end;
+
+procedure TFoundList_deinitialize(PEnv: PJNIEnv; Obj: JObject); cdecl;
+var fl: TFoundList;
+begin
+  fl:=TFoundList(JObjectToTObject(penv, obj));
+  fl.Deinitialize;
+end;
+
+
+procedure TFoundList_refetchValueList(PEnv: PJNIEnv; Obj: JObject); cdecl;
+var fl: TFoundList;
+begin
+  fl:=TFoundList(JObjectToTObject(penv, obj));
+  fl.RefetchValueList;
+end;
+
+
+
+function TFoundList_getCount(PEnv: PJNIEnv; Obj: JObject): jlong; cdecl;
+var
+  extra: dword;
+  value: string;
+begin
+  result:=TFoundList(JObjectToTObject(penv, obj)).count;
+end;
+
+function TFoundList_getAddress(PEnv: PJNIEnv; Obj: JObject; index: jint): jlong; cdecl;
+begin
+  result:=TFoundList(JObjectToTObject(penv, obj)).GetAddress(index);
+end;
+
+function TFoundList_getValue(PEnv: PJNIEnv; Obj: JObject; index: jint): jstring; cdecl;
+var
+  s: string;
+  extra: dword;
+  a: ptruint;
+begin
+  a:=TFoundList(JObjectToTObject(penv, obj)).GetAddress(index, extra, s);
+
+  result:=penv^.NewStringUTF(penv, pchar(s));
+end;
+
+function TFoundList_Create(PEnv: PJNIEnv; Obj: JObject; ownerms: JObject): jlong; cdecl;
+var ms: TMemscan;
+  fl: TFoundList;
+begin
+  log('Creating a foundlist object');
+  ms:=TMemScan(JObjectToTObject(penv, ownerms));
+
+  if (ms<>nil) then
+    log('ms.ScanResultFolder='+ms.ScanresultFolder)
+  else
+    log('ms = nil');
+
+
+  fl:=TFoundList.create(nil, ms);
+
+  result:=ptrint(fl);
+end;
+
+
+const methodcount=7;
+var jnimethods: array [0..methodcount-1] of JNINativeMethod =(
+  (name: 'create'; signature: '(Lorg/cheatengine/TMemScan;)J'; fnPtr: @TFoundList_Create),
+  (name: 'initialize'; signature: '()J'; fnPtr: @TFoundList_initialize),
+  (name: 'deinitialize'; signature: '()V'; fnPtr: @TFoundList_deinitialize),
+  (name: 'getCount'; signature: '()J'; fnPtr: @TFoundList_getCount),
+  (name: 'getAddress'; signature: '(I)J'; fnPtr: @TFoundList_getAddress),
+  (name: 'getValue'; signature: '(I)Ljava/lang/String;'; fnPtr: @TFoundList_getValue),
+  (name: 'refetchValueList'; signature: '()V'; fnPtr: @TFoundList_refetchValueList)
+
+  );
+
+procedure InitializeJniTFoundList(env: PJNIEnv);
+var c: jclass;
+begin
+  log('InitializeJniTFoundList entry');
+  c:=env^.FindClass(env, 'org/cheatengine/TFoundList');
+  env^.RegisterNatives(env, c, @jnimethods[0], methodcount);
+
+  log('InitializeJniTFoundList exit');
+end;
+
+end.
+
