9b7625060bae4e633a974b124d9be8937a749533
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2015-02-13 08:58:08 +0000

    export ParseStringAndWriteToAddress to java

--- Cheat Engine/jni/jnibyteinterpreter.pas
+++ Cheat Engine/jni/jnibyteinterpreter.pas
@@ -11,7 +11,7 @@ procedure InitializeJniByteInterpreter(env: PJNIEnv);
 
 implementation
 
-uses byteinterpreter, commonTypeDefs;
+uses byteinterpreter, commonTypeDefs, unixporthelper;
 
 //function readAndParseAddress(address: ptrUint; variableType: TVariableType; customtype: TCustomType=nil; showashexadecimal: Boolean=false; showAsSigned: boolean=false; bytesize:integer=1): string;
 
@@ -36,19 +36,40 @@ begin
   result:=PEnv^.NewStringUTF(PEnv, pchar(readAndParseAddress(address, Tvariabletype(vtype),nil, showAsHexadecimal<>0, showAsSigned<>0, bytesize)));
 end;
 
-const methodcount=4;
+//parseStringAndWriteToAddress(String value, long address, int variabletype, boolean hexadecimal);
+function byteInterpreter_parseStringAndWriteToAddress(PEnv: PJNIEnv; Obj: JObject; value: jstring; address: jlong; variabletype: integer; hexadecimal: jboolean): jboolean;
+var v: string;
+begin
+  result:=0;
+  try
+    v:=jniGetString(penv, value);
+    ParseStringAndWriteToAddress(v, address, TVariableType(variabletype), hexadecimal<>0);
+    result:=1;
+  except
+    on e: exception do
+    begin
+      log('byteInterpreter_parseStringAndWriteToAddress:'+e.message);
+    end;
+  end;
+end;
+
+const methodcount=5;
 var jnimethods: array [0..methodcount-1] of JNINativeMethod =(
   (name: 'readAndParseAddress'; signature: '(JI)Ljava/lang/String;'; fnPtr: @byteInterpreter_readAndParseAddressJI),
   (name: 'readAndParseAddress'; signature: '(JIZ)Ljava/lang/String;'; fnPtr: @byteInterpreter_readAndParseAddressJIZ),
   (name: 'readAndParseAddress'; signature: '(JIZZ)Ljava/lang/String;'; fnPtr: @byteInterpreter_readAndParseAddressJIZZ),
-  (name: 'readAndParseAddress'; signature: '(JIZZI)Ljava/lang/String;'; fnPtr: @byteInterpreter_readAndParseAddressJIZZI)
-  {
-  address: ptrUint;                        J
-  variableType: TVariableType;             I
-  showashexadecimal: Boolean=false;        Z
-  showAsSigned: boolean=false;             Z
-  bytesize:integer=1): string;             I
-  }
+  (name: 'readAndParseAddress'; signature: '(JIZZI)Ljava/lang/String;'; fnPtr: @byteInterpreter_readAndParseAddressJIZZI),
+  (name: 'parseStringAndWriteToAddress'; signature: '(Ljava/lang/String;JIZ)Z'; fnPtr: @byteInterpreter_parseStringAndWriteToAddress)
+{
+boolean parseStringAndWriteToAddress(String value, long address, int variabletype, boolean hexadecimal);
+  String value
+  long address
+  int variabletype
+  boolean hexadecimal
+
+
+  (Ljava/lang/String;JIZ)Z
+}
 
   );
 
