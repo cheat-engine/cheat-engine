552bddce2402a188fe85ff4b65f8f94b9e249d0a
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2015-02-24 21:08:35 +0000

    implement freezing and deal with the missing network implementation of VirtualProtectEx

--- trunk/Cheat Engine/jni/jniaddresslist.pas
+++ trunk/Cheat Engine/jni/jniaddresslist.pas
@@ -14,10 +14,19 @@ procedure InitializeJniAddressList(env: PJNIEnv);
 
 implementation
 
-uses MemoryRecordUnit, unixporthelper, commonTypeDefs;
+uses MemoryRecordUnit, unixporthelper, commonTypeDefs, syncobjs, math;
+
+type
+  TAddressListFreezer=class(TThread)
+  private
+  public
+    procedure execute; override;
+  end;
 
 var
  addresslist: TList=nil; //list of MemoryRecord's
+ addresslistcs: TCriticalsection; //used to protect the list from the freezer thread. Changing memory records is not part of this.
+
  AddressListEntry_class: jclass; //global ref
  AddressListEntry_init_method: jmethodID;
  description_fieldid: JFieldID;
@@ -25,10 +34,41 @@ var
  addressString_fieldid: JFieldID;
  address_fieldid: JFieldID;
  offsets_fieldid: JFieldID;
+ index_fieldid: JFieldID;
+ active_fieldid: jfieldID;
+
+
+ freezerthread: TAddressListFreezer;
+
+procedure TAddressListFreezer.execute;
+{
+'timer' that freezes the addresslist
+}
+var
+  i: integer;
+  r: TMemoryrecord;
+begin
+  while not terminated do
+  begin
+    addresslistcs.enter;
+    try
+      for i:=0 to addresslist.count-1 do
+      begin
+        r:=TMemoryrecord(addresslist[i]);
+        if (r.Active) then
+          r.ApplyFreeze;
+      end;
+    finally
+      addresslistcs.Leave;
+    end;
+    sleep(100);
+  end;
+end;
+
 
 function addresslist_getCount(PEnv: PJNIEnv; Obj: JObject): jint; cdecl;
 begin
-  log('addresslist_getCount');
+ // log('addresslist_getCount');
   if (addresslist<>nil) then
     result:=addresslist.Count
   else
@@ -38,35 +78,54 @@ begin
   end;
 end;
 
+function addresslist_setActive(PEnv: PJNIEnv; Obj: JObject; index: jint; state: jboolean): jboolean; cdecl;
+var r: TMemoryRecord;
+begin
+  log('addresslist_setActive');
+  if (index>=0) and (index<addresslist.count) then
+  begin
+    r:=TMemoryRecord(addresslist[index]);
+    r.Active:=state=1;
+
+    result:=ifthen(r.Active,1,0);
+
+    if r.Active and (freezerthread=nil) then //start the freezer thread
+      freezerthread:=TAddressListFreezer.Create(false);
+  end;
+end;
+
 function addresslist_getEntry(PEnv: PJNIEnv; Obj: JObject; index: jint): jobject; cdecl;
 var
  r: TMemoryRecord;
  descriptionstring, addressString: jstring;
 begin
   result:=nil;
-  log('addresslist_getEntry');
+  //log('addresslist_getEntry');
   if (index>=0) and (index<addresslist.count) then
   begin
     r:=TMemoryRecord(addresslist[index]);
     result:=penv^.NewObject(penv, AddressListEntry_class, AddressListEntry_init_method);
 
-    log('Created result. Creating description string ('+r.description+')');
+    //log('Created result. Creating description string ('+r.description+')');
     descriptionstring:=penv^.NewStringUTF(penv, pchar(r.Description));
 
-    log('Created descriptionstring, assigning it to the description field');
+   // log('Created descriptionstring, assigning it to the description field');
 
     penv^.SetObjectField(penv, result, description_fieldid, descriptionstring);
 
-    log('Assigned the string to the description field');
+    //log('Assigned the string to the description field');
 
-    log('Creating addressString ('+r.AddressString+')');
+    //log('Creating addressString ('+r.AddressString+')');
     addressString:=penv^.NewStringUTF(penv, pchar(r.AddressString));
-    log('Created addressString ('+r.AddressString+')');
+    //log('Created addressString ('+r.AddressString+')');
 
-    log('Assigning addressString');
+   // log('Assigning addressString');
     penv^.SetObjectField(penv, result, addressString_fieldid, addressString);
 
     penv^.SetLongField(penv, result, address_fieldid, r.GetRealAddress);
+    penv^.SetIntField(penv, result, vartype_fieldid, integer(r.VarType));
+    penv^.SetIntField(penv, result, index_fieldid, index);
+    penv^.SetBooleanField(penv, result, active_fieldid, ifthen(r.active,1,0));
   end;
 end;
 
@@ -82,9 +141,10 @@ var
   offsets: jintArray;
   iscopy: jboolean;
 
+
   i: integer;
 begin
-  log('setting memory record');
+ // log('setting memory record');
   if (index<0) or (index>=addresslist.count) then
   begin
     log('index ('+inttostr(index)+') is outside bounds');
@@ -93,39 +153,41 @@ begin
 
   r:=TMemoryRecord(addresslist[index]);
 
-  log('accessing provided entry');
+ // log('accessing provided entry');
   descriptionstring:=penv^.GetObjectField(penv, entry, description_fieldid);
   if (descriptionstring<>nil) then
   begin
-    log('got the description');
+  //  log('got the description');
     r.Description:=jniGetString(penv, descriptionstring);
 
-    log('description='+r.Description);
+ //   log('description='+r.Description);
   end
   else
     log('description=null');
 
-  log('getting vartype');
-  r.VarType:=TVariableType(penv^.GetIntField(penv, entry, vartype_fieldid));
+ // log('getting vartype');
+  i:=penv^.GetIntField(penv, entry, vartype_fieldid);
+ // log('vartype='+inttostr(i));
+  r.VarType:=TVariableType(i);
 
-  log('Getting addressString');
+ // log('Getting addressString');
   addressString:=penv^.GetObjectField(penv, entry, addressString_fieldid);
   if (addressString<>nil) then
   begin
-    log('got the addressString');
+  //  log('got the addressString');
     r.interpretableaddress:=jniGetString(penv, addressString);
   end
   else
-    log('description=null');
+    log('addressString=null');
 
 
   offsets:=penv^.GetObjectField(penv, entry, offsets_fieldid);
   if (offsets<>nil) then
   begin
-    log('offsets is not null');
+  //  log('offsets is not null');
 
     l:=penv^.GetArrayLength(penv, offsets);
-    log('the offsets array length='+inttostr(l));
+  //  log('the offsets array length='+inttostr(l));
 
     offsetlist:=PJintArray(penv^.GetIntArrayElements(penv,offsets, iscopy));
 
@@ -134,9 +196,8 @@ begin
       r.pointeroffsets[i]:=offsetlist[i];
 
     penv^.ReleaseIntArrayElements(penv, offsets, PJint(offsetlist),0);
-  end
-  else
-    log('offsets=null');
+  end;
+
 
   r.ReinterpretAddress;
 end;
@@ -145,42 +206,62 @@ function addresslist_addEntry(PEnv: PJNIEnv; Obj: JObject; entry: JObject): jint
 var
   r: TMemoryrecord;
 begin
-  log('creating memory record');
+  //log('creating memory record');
   r:=TMemoryrecord.Create(nil);
-  addresslist.Add(r);
-  result:=addresslist.count;
+  addresslistcs.enter;
+  try
+    addresslist.Add(r);
+    result:=addresslist.count;
 
-  log('Initializing memory record. (result='+inttostr(result)+')');
-  addresslist_setEntry(penv, obj, result-1, entry);
+   // log('Initializing memory record. (result='+inttostr(result)+')');
+    addresslist_setEntry(penv, obj, result-1, entry);
+
+  finally
+    addresslistcs.leave;
+  end;
+
 end;
 
 procedure addresslist_deleteEntry(PEnv: PJNIEnv; Obj: JObject; index: jint); cdecl;
 begin
   log('Delete entry');
+
   if (index>=0) and (index<addresslist.count) then
   begin
-    TMemoryRecord(addresslist[index]).free;
-    addresslist.Delete(index);
+    addresslistcs.enter;
+    try
+      TMemoryRecord(addresslist[index]).free;
+      addresslist.Delete(index);
+    finally
+      addresslistcs.leave;
+    end;
   end;
+
 end;
 
 procedure addresslist_clear(PEnv: PJNIEnv; Obj: JObject); cdecl;
 var i: integer;
 begin
   log('addresslist.Clear');
-  for i:=0 to addresslist.count-1 do
-    TMemoryrecord(addresslist[i]).Free;
+  addresslistcs.enter;
+  try
+    for i:=0 to addresslist.count-1 do
+      TMemoryrecord(addresslist[i]).Free;
 
-  addresslist.Clear;
+    addresslist.Clear;
+  finally
+    addresslistcs.leave;
+  end;
 end;
 
 
-const methodcount=6;
+const methodcount=7;
 var jnimethods: array [0..methodcount-1] of JNINativeMethod =(
   (name: 'GetCount'; signature: '()I'; fnPtr: @addresslist_getCount),
   (name: 'GetEntry'; signature: '(I)Lorg/cheatengine/AddressListEntry;'; fnPtr: @addresslist_getEntry),
   (name: 'SetEntry'; signature: '(ILorg/cheatengine/AddressListEntry;)V'; fnPtr: @addresslist_setEntry),
   (name: 'AddEntry'; signature: '(Lorg/cheatengine/AddressListEntry;)I'; fnPtr: @addresslist_addEntry),
+  (name: 'SetEntryActive'; signature: '(IZ)Z'; fnPtr: @addresslist_setActive),
   (name: 'DeleteEntry'; signature: '(I)V'; fnPtr: @addresslist_deleteEntry),
   (name: 'Clear'; signature: '()V'; fnPtr: @addresslist_clear)
 
@@ -190,7 +271,10 @@ procedure InitializeJniAddressList(env: PJNIEnv);
 var c: jclass;
 begin
   if addresslist=nil then
+  begin
     addresslist:=Tlist.create;
+    addresslistcs:=TCriticalSection.Create;
+  end;
 
   c:=env^.FindClass(env, 'org/cheatengine/AddressList');
   env^.RegisterNatives(env, c, @jnimethods[0], methodcount);
@@ -204,6 +288,9 @@ begin
   addressString_fieldid:=env^.GetFieldID(env, AddressListEntry_class, 'addressString','Ljava/lang/String;');
   address_fieldid:=env^.GetFieldID(env, AddressListEntry_class, 'address','J');
   offsets_fieldid:=env^.GetFieldID(env, AddressListEntry_class, 'offsets','[I');
+  active_fieldid:=env^.GetFieldID(env, AddressListEntry_class, 'active','Z');
+
+  index_fieldid:=env^.GetFieldID(env, AddressListEntry_class, 'index','I');
 
   log('description_fieldid='+inttohex(ptruint(description_fieldid),8));
   log('vartype_fieldid='+inttohex(ptruint(vartype_fieldid),8));
