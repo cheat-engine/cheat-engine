5affea898020c4fa32019b9897ab716df6af5547
cheatengine@gmail.com <cheatengine@gmail.com@16004826-78b5-1967-6829-2a7ac34a5c3b>
2015-04-01 10:57:04 +0000

    making the config a bit easier

--- Cheat Engine/ceserver/Release-android/subdir.mk
+++ Cheat Engine/ceserver/Release-android/subdir.mk
@@ -38,7 +38,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	/android-build/bin/arm-linux-androideabi-gcc -O3 -Wall -c -fmessage-length=0 -fPIE -pie -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-linux-androideabi-gcc -O3 -Wall -c -fmessage-length=0 -fPIE -pie -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
