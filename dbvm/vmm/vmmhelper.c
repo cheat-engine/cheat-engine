abf39169e5725c115531f95ead967f8ab9ae5f42
Eric Heijnen <dark_byte@hotmail.com>
2015-10-07 11:22:19 +0200

    undo some testcode

--- dbvm/vmm/vmmhelper.c
+++ dbvm/vmm/vmmhelper.c
@@ -2164,8 +2164,8 @@ void setupVMX(pcpuinfo currentcpuinfo)
 
   csEnter(&setupVMX_lock);
 
-//  currentcpuinfo->AvailableVirtualAddress=(UINT64)(currentcpuinfo->cpunr+1) << 28;
-  currentcpuinfo->AvailableVirtualAddress=(UINT64)(currentcpuinfo->cpunr+16) << 28;
+  currentcpuinfo->AvailableVirtualAddress=(UINT64)(currentcpuinfo->cpunr+1) << 28;
+//  currentcpuinfo->AvailableVirtualAddress=(UINT64)(currentcpuinfo->cpunr+16) << 28;
   sendstringf("AvailableVirtualAddress=%6\n\r",currentcpuinfo->AvailableVirtualAddress);
 
   if (!loadedOS)
