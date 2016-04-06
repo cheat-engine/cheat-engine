b924d4f248fd1646358f6b8f30d25df1dcda11c8
Charles Grunwald <ch@rles.rocks>
2016-04-06 02:31:52 -0700

    Fix parameter handling in executeCode

--- Cheat Engine/LuaHandler.pas
+++ Cheat Engine/LuaHandler.pas
@@ -6251,9 +6251,9 @@ begin
     if lua_gettop(L)>=2 then
     begin
       if lua_isnumber(L,2) then
-        address:=lua_tointeger(L, 2)
+        parameter:=lua_tointeger(L, 2)
       else
-        address:=selfsymhandler.getAddressFromName(Lua_ToString(L,2));
+        parameter:=selfsymhandler.getAddressFromName(Lua_ToString(L,2));
     end
     else
       parameter:=0;
