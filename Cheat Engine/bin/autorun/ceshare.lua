59d5f068aea5526a1c7cc14beb20d5f710f930a5
Dark Byte <dark_byte@hotmail.com>
2016-10-09 02:05:19 +0200

    logout

--- Cheat Engine/bin/autorun/ceshare.lua
+++ Cheat Engine/bin/autorun/ceshare.lua
@@ -1,8 +1,15 @@
-package.path=package.path..';'..getCheatEngineDir()..[[autorun\xml\?.lua]]
+if package.loaded.xmlSimple==nil then
+  package.path=package.path..';'..getCheatEngineDir()..[[autorun\xml\?.lua]]
+else
+  package.loaded.xmlSimple=nil
+end
 xmlParser = require("xmlSimple").newParser()
 
 
+
 ceshare={}
+ceshare.version=-1
+ceshare.base='http://cheatengine.org/ceshare/'
 
 function ceshare:url_encode(str)
   if (str) then
@@ -23,7 +30,7 @@ end
 
 function ceshare:login(username,password)
   local i=self:getInternet()  
-  local r=i.postURL('http://cheatengine.org/ceshare/login.php','username='..ceshare:url_encode(username)..'&password='..ceshare:url_encode(password))
+  local r=i.postURL(self.base..'login.php','username='..self:url_encode(username)..'&password='..self:url_encode(password))
   if (r:sub(1,1)=='<') then
     local s=xmlParser:ParseXmlText(r)
     if s then
@@ -40,5 +47,28 @@ function ceshare:login(username,password)
     end
   else
     showMessage(r);
+  end
+end
+
+function ceshare:logout()
+  if self.sessionID then
+    local r=self:getInternet().postURL(self.base..'logout.php','sessionid='..self.sessionID)
+    if (r:sub(1,1)=='<') then
+      local s=xmlParser:ParseXmlText(r)
+      if s then
+        if s.Session then
+          self.sessionID=nil
+          return true
+        else
+          if s.error then
+            showMessage(s.error:value())
+          end
+        end
+      else
+        showMessage(r)
+      end
+    else
+      showMessage(r);
+    end
   end
 end
