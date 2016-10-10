f40be7a21175729188758741cdf842b32af8d355
Dark Byte <dark_byte@hotmail.com>
2016-10-11 01:52:57 +0200

    some moving around of stuff for better performance/usability

--- Cheat Engine/bin/autorun/ceshare.lua
+++ Cheat Engine/bin/autorun/ceshare.lua
@@ -72,3 +72,68 @@ function ceshare:logout()
     end
   end
 end
+
+function ceshare:QueryProcessCheats(processnamemd5, headermd5)
+  --print("processnamemd5="..processnamemd5.." headermd5="..headermd5)
+  local result=nil
+  local r=self:getInternet().postURL(self.base..'QueryProcessCheats.php','processnamemd5='..processnamemd5..'&headermd5='..headermd5)
+  if (r:sub(1,1)=='<') then
+    local s=xmlParser:ParseXmlText(r)
+    if s then
+      if s.CheatList then
+        --parse the list
+        local i
+        if s.CheatList.CheatEntry then
+          --there are results
+          result={}
+          for i=1, #s.CheatList.CheatEntry do
+            local CheatEntry=s.CheatList.CheatEntry[i]
+            if CheatEntry then
+              local entry={}
+              entry.ID=tonumber(CheatEntry["@ID"])
+              entry.Owner=CheatEntry["@Owner"]
+              entry.VersionIndependant=CheatEntry["@VersionIndependant"]=1
+              entry.Public=CheatEntry["@Public"]=1
+              entry.Rating=tonumber(CheatEntry["@Rating"])
+              entry.RatingCount=tonumber(CheatEntry["@RatingCount"])
+              entry.LastUpdate=os.date("*t", CheatEntry["@LastUpdate"])                            
+              entry.FullFileHash=CheatEntry["@FullFileHash"]
+              entry.SecondaryModuleName=CheatEntry["@SecondaryModuleName"]
+              entry.SecondaryFullFileHash=CheatEntry["@SecondaryFullFileHash"]
+              entry.CheckCode=CheatEntry["@CheckCode"]
+              entry.Description=CheatEntry["@Description"]
+              entry.DataType=CheatEntry["@DataType"]              
+              
+              table.insert(result, entry)
+            end
+          
+          end
+        end
+     
+      else
+        if s.error then
+          showMessage(s.error:value())
+        end
+      end                
+    else
+      showMessage(r);
+    end    
+  else
+    showMessage(r);
+  end
+  
+  return result
+end
+
+function ceshare:QueryCurrentProcess()
+  local pid=getOpenedProcessID()
+  if (pid) and (pid~=0) then
+    local modulelist=enumModules()
+    if (modulelist) and (#modulelist>0) then
+      local processnamemd5=modulelist[1].Name:lower()
+      local headermd5=md5memory(modulelist[1].Address,4096)
+      self:QueryProcessCheats(processnamemd5, headermd5)
+    end
+  end
+  
+end
