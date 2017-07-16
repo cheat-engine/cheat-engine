b73112773bb814aafacec43aa76ccc0e5298cece
Dark Byte <dark_byte@hotmail.com>
2017-07-16 10:47:32 +0200

    better fix

--- Cheat Engine/bin/autorun/luasymbols.lua
+++ Cheat Engine/bin/autorun/luasymbols.lua
@@ -1,12 +1,20 @@
 luasymbols=registerSymbolLookupCallback(function(str)
-  local c='return [['..str..']]'
-  local lc=loadstring(c)
-  if lc then
-    local isvalid,result=pcall(lc)
-    if isvalid then
-      return result
-    else
+  if str then
+    local firstchar=str:sub(1,1)
+    
+    if (firstchar=='\'') or (firstchar=='\"') then
       return nil
+    end
+  
+    local c='return '..str
+    local lc=loadstring(c)
+    if lc then
+      local isvalid,result=pcall(lc)
+      if isvalid then
+        return result
+      else
+        return nil
+      end
     end
   end
 end, slNotSymbol) 
