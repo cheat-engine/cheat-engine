75f8d95bedd1aacc577766c3ec6b2b73cea16112
Dark Byte <dark_byte@hotmail.com>
2018-12-18 01:27:46 +0100

    forgot to add the codefiltercalloralldialog

---
+++ Cheat Engine/codefiltercalloralldialog.pas
@@ -0,0 +1,32 @@
+unit CodeFilterCallOrAllDialog;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TCallOrAllDialog }
+
+  TCallOrAllDialog = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Label1: TLabel;
+    Panel1: TPanel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
