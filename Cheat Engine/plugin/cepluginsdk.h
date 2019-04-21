18bc882b18bdc1d617dc1596c1e90aba8f38cb20
Dark Byte <dark_byte@hotmail.com>
2019-04-21 08:52:54 +0200

    some more type fixes for the plugin sdk

--- Cheat Engine/plugin/cepluginsdk.h
+++ Cheat Engine/plugin/cepluginsdk.h
@@ -38,7 +38,7 @@ typedef struct _PLUGINTYPE0_RECORD
 
 //callback routines efinitions for registered plugin functions:
 typedef BOOL (__stdcall *CEP_PLUGINTYPE0)(PPLUGINTYPE0_RECORD SelectedRecord);
-typedef BOOL (__stdcall *CEP_PLUGINTYPE1)(ULONG *disassembleraddress, ULONG *selected_disassembler_address, ULONG *hexviewaddress);
+typedef BOOL (__stdcall *CEP_PLUGINTYPE1)(UINT_PTR *disassembleraddress, UINT_PTR *selected_disassembler_address, UINT_PTR *hexviewaddress);
 typedef int (__stdcall *CEP_PLUGINTYPE2)(LPDEBUG_EVENT DebugEvent);
 typedef void (__stdcall *CEP_PLUGINTYPE3)(ULONG processid, ULONG peprocess, BOOL Created);
 typedef void (__stdcall *CEP_PLUGINTYPE4)(int reserved);
@@ -182,10 +182,10 @@ typedef BOOL (__stdcall *CEP_NAMETOADDRESS) (char *name, UINT_PTR *address);
 
 typedef VOID (__stdcall *CEP_LOADDBK32)(void);
 typedef BOOL (__stdcall *CEP_LOADDBVMIFNEEDED)(void);
-typedef DWORD (__stdcall *CEP_PREVIOUSOPCODE)(ULONG address);
-typedef DWORD (__stdcall *CEP_NEXTOPCODE)(ULONG address);
+typedef DWORD (__stdcall *CEP_PREVIOUSOPCODE)(UINT_PTR address);
+typedef DWORD (__stdcall *CEP_NEXTOPCODE)(UINT_PTR address);
 typedef BOOL (__stdcall *CEP_LOADMODULE)(char *modulepath, char *exportlist, int *maxsize);
-typedef BOOL (__stdcall *CEP_DISASSEMBLEEX)(ULONG address, char *output, int maxsize);
+typedef BOOL (__stdcall *CEP_DISASSEMBLEEX)(UINT_PTR address, char *output, int maxsize);
 typedef VOID (__stdcall *CEP_AA_ADDCOMMAND)(char *command);
 typedef VOID (__stdcall *CEP_AA_DELCOMMAND)(char *command);
 
