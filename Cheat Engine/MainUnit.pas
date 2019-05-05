c1654f4e22673b5495a940127a78add539f513bc
Dark Byte <dark_byte@hotmail.com>
2019-05-05 17:56:38 +0200

    ok, no savebutton adjust needed

--- Cheat Engine/MainUnit.pas
+++ Cheat Engine/MainUnit.pas
@@ -7571,7 +7571,6 @@ begin
 
   sbOpenProcess.BorderSpacing.Around:=ScaleX(sbOpenProcess.BorderSpacing.Around, 96);
   loadbutton.BorderSpacing.Top:=sbOpenProcess.BorderSpacing.Around;
-  savebutton.BorderSpacing.Top:=sbOpenProcess.BorderSpacing.Around;
 
 
   //because the images have no empty border autosize is out of the question as that makes them hug the border. So, scale them manually
