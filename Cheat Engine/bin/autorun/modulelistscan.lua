2386924407b895d81392b3eae1f97e08112d1888
Dark Byte <dark_byte@hotmail.com>
2019-09-29 22:54:11 +0200

    fill on mouseenter

--- Cheat Engine/bin/autorun/modulelistscan.lua
+++ Cheat Engine/bin/autorun/modulelistscan.lua
@@ -1,4 +1,4 @@
-c=createComboBox(MainForm.gbScanOptions)
+local c=createComboBox(MainForm.gbScanOptions)
 
 c.Style='csDropDownList'
 c.Items.add('All')
@@ -12,8 +12,7 @@ c.BorderSpacing.Bottom=2
 
 local modulelist
 
-c.OnDropDown=function(d)
-  --fill the list
+function FillList()
   while c.Items.Count>1 do
     c.Items.delete(1)
   end
@@ -23,6 +22,19 @@ c.OnDropDown=function(d)
   for i=1, #modulelist do
     c.Items.Add(modulelist[i].Name)
   end
+end
+
+c.OnMouseEnter=function(d)
+  
+  if c.Items.Count<=1 then
+    print("enter")
+    FillList()
+    c.ItemIndex=0
+  end
+end
+
+c.OnDropDown=function(d)
+  FillList()
 end
 
 c.OnSelect=function(d)
