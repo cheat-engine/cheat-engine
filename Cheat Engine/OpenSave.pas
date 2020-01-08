7d04b2c601140abb479b1e9292db2e5d765a4c89
Dark Byte <dark_byte@hotmail.com>
2020-01-09 00:58:17 +0100

    fix

--- Cheat Engine/OpenSave.pas
+++ Cheat Engine/OpenSave.pas
@@ -318,7 +318,7 @@ var
     imagepos: integer=0;
 begin
   LUA_DoScript('tableIsLoading=true');
-  LUA_DoScript('onTableLoad(true)');
+  LUA_functioncall('onTableLoad',[true]);
 
   try
     signed:=false;
@@ -894,7 +894,8 @@ begin
 
   finally
     LUA_DoScript('tableIsLoading=false');
-    LUA_DoScript('onTableLoad(false)');
+
+    LUA_functioncall('onTableLoad',[false]);
 
     if image<>nil then
       freeandnil(image);
