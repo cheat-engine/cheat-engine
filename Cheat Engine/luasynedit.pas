186fa170e43b92f133eafff2d64564ccdc7b8312
Dark Byte <dark_byte@hotmail.com>
2020-03-05 15:22:14 +0100

    add SelEnd, SelStart and SelText

--- Cheat Engine/luasynedit.pas
+++ Cheat Engine/luasynedit.pas
@@ -39,12 +39,79 @@ begin
 
     luaclass_newClass(L, s);
     result:=1;
+
+
   end;
 end;
+
+
+function syn_getSelStart(L: PLua_State): integer; cdecl;
+var
+  s: TCustomSynEdit;
+begin
+  s:=luaclass_getClassObject(L);
+  lua_pushinteger(L, s.SelStart);
+  result:=1;
+end;
+
+function syn_setSelStart(L: PLua_State): integer; cdecl;
+var
+  s: TCustomSynEdit;
+begin
+  s:=luaclass_getClassObject(L);
+  if lua_gettop(L)>0 then
+    s.SelStart:=lua_tointeger(L,1);
+
+  result:=0;
+end;
+
+function syn_getSelEnd(L: PLua_State): integer; cdecl;
+var
+  s: TCustomSynEdit;
+begin
+  s:=luaclass_getClassObject(L);
+  lua_pushinteger(L, s.SelEnd);
+  result:=1;
+end;
+
+function syn_setSelEnd(L: PLua_State): integer; cdecl;
+var
+  s: TCustomSynEdit;
+begin
+  s:=luaclass_getClassObject(L);
+  if lua_gettop(L)>0 then
+    s.SelEnd:=lua_tointeger(L,1);
+  result:=0;
+end;
+
+function syn_getSelText(L: PLua_State): integer; cdecl;
+var
+  s: TCustomSynEdit;
+begin
+  s:=luaclass_getClassObject(L);
+  lua_pushstring(L, s.SelText);
+  result:=1;
+end;
+
+function syn_setSelText(L: PLua_State): integer; cdecl;
+var
+  s: TCustomSynEdit;
+begin
+  s:=luaclass_getClassObject(L);
+  if lua_gettop(L)>0 then
+    s.SelText:=lua_tostring(L, 1);
+
+  result:=0;
+end;
+
 
 procedure luasynedit_addMetaData(L: PLua_state; metatable: integer; userdata: integer );
 begin
   customcontrol_addMetaData(L, metatable, userdata);
+
+  luaclass_addPropertyToTable(L, metatable, userdata, 'SelStart', syn_getSelStart, syn_setSelStart);
+  luaclass_addPropertyToTable(L, metatable, userdata, 'SelEnd', syn_getSelEnd, syn_setSelEnd);
+  luaclass_addPropertyToTable(L, metatable, userdata, 'SelText', syn_getSelText, syn_setSelText);
 
 end;
 
