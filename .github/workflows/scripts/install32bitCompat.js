54fdca0cb57c5142d4ed53753af807a05c1dd8d0
Ardit <ardittristan@gmail.com>
2022-04-07 01:59:21 +0200

    remove npm dependencies

--- .github/workflows/scripts/install32bitCompat.js
+++ .github/workflows/scripts/install32bitCompat.js
@@ -1,11 +1,25 @@
 const { join } = require("path");
-const { execSync } = require("child_process");
-const fetchSync = require("@the-bds-maneger/fetchsync");
+const { exec } = require("child_process");
+const { createWriteStream } = require("fs");
+const { get: _get } = require("https");
 
-fetchSync(
+get(
   `https://downloads.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%20${process.env.LAZARUS_VERSION}/lazarus-${process.env.LAZARUS_VERSION}-fpc-${process.env.FPC_VERSION}-cross-i386-win32-win64.exe`,
-  {
-    Binary: true,
+  (res) => {
+    const download = createWriteStream(join(process.env.GITHUB_WORKSPACE, "32bit.exe"));
+    res.pipe(download);
+    download.on("finish", () => {
+      exec(`cd "${process.env.GITHUB_WORKSPACE}" && 32bit.exe /VERYSILENT /DIR=${join(process.env.RUNNER_TEMP, "lazarus")}`);
+    });
   }
-).save(join(process.env.GITHUB_WORKSPACE, "32bit.exe"));
-execSync(`cd "${process.env.GITHUB_WORKSPACE}" && 32bit.exe /VERYSILENT /DIR=${join(process.env.RUNNER_TEMP, "lazarus")}`);
+);
+
+function get(url, callback) {
+  _get(url, (res) => {
+    if (res.statusCode === 301 || res.statusCode === 302 || res.statusCode === 307 || res.statusCode === 308) {
+      get(res.headers.location, callback);
+    } else {
+      callback(res);
+    }
+  });
+}
