ad79d2471f2d432db2415bb22f0f8716fcd75d9f
tiann <twsxtd@gmail.com>
2022-11-24 19:06:15 +0800

    fix ndk-build failed of options.c

--- Cheat Engine/ceserver/ndk-build/EXECUTABLE/jni/Android.mk
+++ Cheat Engine/ceserver/ndk-build/EXECUTABLE/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_CFLAGS += -fPIE -DBUILD_OPTION=0
 LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true
 LOCAL_MODULE    := ceserver
-LOCAL_SRC_FILES :=  ../../../api.c ../../../ceserver.c ../../../porthelp.c ../../../symbols.c ../../../threads.c ../../../context.c ../../../ceservertest.c ../../../extensionfunctions.c ../../../extensionloader.c ../../../native-api.c
+LOCAL_SRC_FILES :=  ../../../api.c ../../../ceserver.c ../../../porthelp.c ../../../symbols.c ../../../threads.c ../../../context.c ../../../ceservertest.c ../../../extensionfunctions.c ../../../extensionloader.c ../../../native-api.c ../../../options.c
 LOCAL_LDLIBS := -lz -llog
 
 include $(BUILD_EXECUTABLE)
