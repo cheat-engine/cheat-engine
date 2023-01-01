2fc632eb5dab13883868df9f979df785e7678f93
cheat-engine <dark_byte@hotmail.com>
2023-01-01 13:44:26 +0000

    add a more descriptive error message when the driver is blocked from loading

--- Cheat Engine/dbk32/DBK32functions.pas
+++ Cheat Engine/dbk32/DBK32functions.pas
@@ -454,6 +454,9 @@ resourcestring
   rsAPCRules = 'APC rules';
   rsPleaseRunThe64BitVersionOfCE = 'Please run the 64-bit version of '+strCheatEngine;
   rsDBKError = 'DBK Error';
+  rsDBKBlockedDueToVulnerableDriverBlocklist = 'Failure starting dbk because '
+    +'the vulnerable driver blocklist is enabled and dbk has been added to it.'
+    +' Want to know how to disable this?';
 
 var dataloc: widestring;
     applicationPath: widestring;
@@ -3395,7 +3398,17 @@ begin
             if le<>1056 then
             begin
               if dbvm_version=0 then
-                messagebox(0,PChar('Failure starting dbk:'+inttostr(le)),PChar(rsDbk32Error),MB_ICONERROR or mb_ok);
+              begin
+                if dword(le)=$800B010C then
+                begin
+                  if messagebox(0, PChar(rsDBKBlockedDueToVulnerableDriverBlocklist), pchar(rsDbk32Error), MB_ICONERROR or MB_YESNO)=IDYES then
+                  begin
+                    shellexecute(0, 'open', 'https://cheatengine.org/dbkerror.php', nil, nil, sw_show);
+                  end;
+                end
+                else
+                  messagebox(0,PChar('Failure starting dbk:'+inttostr(le)),PChar(rsDbk32Error),MB_ICONERROR or mb_ok);
+              end;
             end;
           end;
 
